715de726132aaae1e6463e08f00846660556a119
Timo Michna <timomichna@yahoo.de>
2021-10-12 07:39:53 +0200

    Add symfony bundle class

---
+++ instrumentation/symfony/OpenTelemetryBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Instrumentation\Symfony\OpenTelemetryBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OpenTelemetryBundle extends Bundle
+{
+    public function build(ContainerBuilder $container): void
+    {
+
+    }
+}
