e269786b03ee8f1a23f5a25c3cb00e551f44d236
Timo Michna <timomichna@yahoo.de>
2021-10-19 01:16:50 +0200

    Add ContainerConfiguratorHelper

---
+++ tests/unit/instrumentation/Symfony/OtelSdkBundle/Util/ContainerConfiguratorHelperTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Test\Unit\Symfony\OtelSdkBundle\Util;
+
+use DG\BypassFinals;
+use OpenTelemetry\Instrumentation\Symfony\OtelSdkBundle\Util\ContainerConfiguratorHelper;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ServiceConfigurator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
+
+class ContainerConfiguratorHelperTest extends TestCase
+{
+    public function setUp(): void
+    {
+        BypassFinals::enable();
+    }
+
+    public function testCreate()
+    {
+        $this->assertInstanceOf(
+            ContainerConfiguratorHelper::class,
+            ContainerConfiguratorHelper::create(
+                $this->createContainerConfiguratorMock()
+            )
+        );
+    }
+
+    public function testGetServicesConfigurator()
+    {
+        $this->assertInstanceOf(
+            ServicesConfigurator::class,
+            ContainerConfiguratorHelper::create(
+                $this->createContainerConfiguratorMock()
+            )->getServicesConfigurator()
+        );
+    }
+
+    public function testSetService()
+    {
+        $this->assertInstanceOf(
+            ServiceConfigurator::class,
+            ContainerConfiguratorHelper::create(
+                $this->createContainerConfiguratorMock()
+            )->setService(__CLASS__, false)
+        );
+    }
+
+    public function testSetServiceWithAlias()
+    {
+        $this->assertInstanceOf(
+            ServiceConfigurator::class,
+            ContainerConfiguratorHelper::create(
+                $this->createContainerConfiguratorMock()
+            )->setService(__CLASS__, true)
+        );
+    }
+
+    /**
+     * @return ContainerConfigurator
+     */
+    private function createContainerConfiguratorMock(): ContainerConfigurator
+    {
+        $mock =  $this->getMockBuilder(ContainerConfigurator::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mock->expects($this->any())
+            ->method('services')
+            ->willReturnCallback(function () {
+                return $this->createMock(ServicesConfigurator::class);
+            });
+
+        return $mock;
+    }
+}
+
