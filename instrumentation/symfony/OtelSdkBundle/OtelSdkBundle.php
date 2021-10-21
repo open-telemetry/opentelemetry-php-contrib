8fe519a5568f36178dbf6167de643b0f4342547c
Timo Michna <timomichna@yahoo.de>
2021-10-21 03:33:46 +0200

    Remove unused class import

--- instrumentation/symfony/OtelSdkBundle/OtelSdkBundle.php
+++ instrumentation/symfony/OtelSdkBundle/OtelSdkBundle.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Instrumentation\Symfony\OtelSdkBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OtelSdkBundle extends Bundle
