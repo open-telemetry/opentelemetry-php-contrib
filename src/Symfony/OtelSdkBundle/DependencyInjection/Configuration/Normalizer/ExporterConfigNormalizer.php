0a2036d822a0c6ed8a7e4e0ac9c74b46cf19d76c
Timo Michna <timomichna@yahoo.de>
2022-04-18 22:55:46 +0200

    Reduce cyclomatic complexity

--- src/Symfony/OtelSdkBundle/DependencyInjection/Configuration/Normalizer/ExporterConfigNormalizer.php
+++ src/Symfony/OtelSdkBundle/DependencyInjection/Configuration/Normalizer/ExporterConfigNormalizer.php
@@ -16,42 +16,64 @@ class ExporterConfigNormalizer
     public static function normalize($config): array
     {
         if (is_array($config)) {
-            // exporter is set via - dsn: type+scheme://host:123/path
-            if (isset($config[ConfigurationInterface::DSN_NODE])) {
-                $dsn = $config[ConfigurationInterface::DSN_NODE];
-                if (is_string($dsn)) {
-                    return self::normalize($dsn);
-                }
-
-                throw new ConfigurationException(
-                    'Exporter configuration "dsn" value must be a string'
-                );
-            }
-            if (isset($config[ConfigurationInterface::TYPE_NODE])) {
-                TypedExporterConfigValidator::approve($config);
-
-                return $config;
-            }
-
-            throw new ConfigurationException(
-                'Exporter configuration must either have a key "dsn", keys "type"+"url" or type=custom and keys "class" or "id". given: '
-                . print_r($config, true)
-            );
+            return self::normalizeArray($config);
         }
-        if (is_string($config)) {
-            if (self::isEnvVarReference($config)) {
-                return [
-                    ConfigurationInterface::TYPE_NODE => ConfigurationInterface::ENV_TYPE,
-                    ConfigurationInterface::URL_NODE => $config,
-                ];
-            }
 
-            return self::exporterDsnToArray($config);
+        if (is_string($config)) {
+            return self::normalizeString($config);
         }
 
         throw new ConfigurationException(
             'Exporter configuration must be either a dsn or an array'
         );
+    }
+
+    private static function normalizeString(string $config): array
+    {
+        if (self::isEnvVarReference($config)) {
+            return [
+                ConfigurationInterface::TYPE_NODE => ConfigurationInterface::ENV_TYPE,
+                ConfigurationInterface::URL_NODE => $config,
+            ];
+        }
+
+        return self::exporterDsnToArray($config);
+    }
+
+    private static function normalizeArray(array $config): array
+    {
+        // exporter is set via - dsn: type+scheme://host:123/path
+        if (isset($config[ConfigurationInterface::DSN_NODE])) {
+            return self::normalizeDsnNode($config);
+        }
+
+        if (isset($config[ConfigurationInterface::TYPE_NODE])) {
+            return self::normalizeTypedNode($config);
+        }
+
+        throw new ConfigurationException(
+            'Exporter configuration must either have a key "dsn", keys "type"+"url" or type=custom and keys "class" or "id". given: '
+            . print_r($config, true)
+        );
+    }
+
+    private static function normalizeDsnNode(array $config): array
+    {
+        $dsn = $config[ConfigurationInterface::DSN_NODE];
+        if (is_string($dsn)) {
+            return self::normalize($dsn);
+        }
+
+        throw new ConfigurationException(
+            'Exporter configuration "dsn" value must be a string'
+        );
+    }
+
+    private static function normalizeTypedNode(array $config): array
+    {
+        TypedExporterConfigValidator::approve($config);
+
+        return $config;
     }
 
     private static function exporterDsnToArray(string $config): array
