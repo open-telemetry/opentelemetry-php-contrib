33359137e9c868f08372c99a5d205cc9e6ac1704
Brett McBride <brett.a.mcbride@gmail.com>
2022-11-29 14:41:57 +1100

    migrate context/swoole from main repo (#87)
    copied source from opentelemetry-php into -contrib

---
+++ src/Context/Swoole/tests/Unit/SwooleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Context\Swoole\tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversNothing
+ */
+class SwooleTest extends TestCase
+{
+    public function test_dummy(): void
+    {
+        $this->markTestSkipped('no tests exist for Context/Swoole');
+    }
+}
