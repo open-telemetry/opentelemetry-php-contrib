6fa56d5a31dcb0d505ac4652d2816a23359c30f4
Brett McBride <brett@deakin.edu.au>
2022-12-19 09:57:06 +1100

    add comment about what happens on routing failure

--- src/Instrumentation/Slim/src/SlimInstrumentation.php
+++ src/Instrumentation/Slim/src/SlimInstrumentation.php
@@ -30,6 +30,7 @@ class SlimInstrumentation
          * This relies upon the existence of a request attribute with key SpanInterface::class
          * and type SpanInterface which represents the root span, having been previously set (eg
          * by PSR-15 auto-instrumentation)
+         * If routing fails (eg 404/not found), then the root span name will not be updated.
          *
          * @psalm-suppress ArgumentTypeCoercion
          */
