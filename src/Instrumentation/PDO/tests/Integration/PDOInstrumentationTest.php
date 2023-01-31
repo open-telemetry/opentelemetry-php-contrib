912676f56cf428da9153543b4896575d3473f9be
Przemek Delewski <pdelewski@sumologic.com>
2023-01-31 12:56:35 +0100

    PDO initial integration test

---
+++ src/Instrumentation/PDO/tests/Integration/PDOInstrumentationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Tests\Instrumentation\PDO\tests\Integration;
+
+use ArrayObject;
+use Mockery;
+use OpenTelemetry\API\Common\Instrumentation\Configurator;
+use OpenTelemetry\Context\ScopeInterface;
+use OpenTelemetry\SDK\Trace\ImmutableSpan;
+use OpenTelemetry\SDK\Trace\SpanExporter\InMemoryExporter;
+use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
+use OpenTelemetry\SDK\Trace\TracerProvider;
+use PHPUnit\Framework\TestCase;
+
+class PDOInstrumentationTest extends TestCase
+{
+    
+    private ScopeInterface $scope;
+    private ArrayObject $storage;
+    private TracerProvider $tracerProvider;
+
+    public function setUp(): void
+    {
+        $this->storage = new ArrayObject();
+        $tracerProvider = new TracerProvider(
+            new SimpleSpanProcessor(
+                new InMemoryExporter($this->storage)
+            )
+        );
+
+        $this->scope = Configurator::create()
+            ->withTracerProvider($tracerProvider)
+            ->activate();
+    }
+
+    public function tearDown(): void
+    {
+        $this->scope->detach();
+    }
+
+    public function test_pdo_construct(): void
+    {
+        // @var ImmutableSpan $span
+        $this->assertCount(0, $this->storage);
+        $pdo =  new \PDO('sqlite::memory:');
+        $this->assertCount(1, $this->storage);
+        $span = $this->storage->offsetGet(0);
+        var_dump($span->getName());
+        $this->assertSame('PDO::__construct', $span->getName());
+    }
+
+}
