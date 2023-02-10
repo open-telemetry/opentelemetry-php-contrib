9c7b5528d1881844644331f545efc9cd24052301
Przemyslaw Delewski <102958445+pdelewski@users.noreply.github.com>
2023-02-10 06:54:46 +0100

    PDO auto instrumentation #2, adding more operations and extending tests (#109)

--- src/Instrumentation/PDO/tests/Integration/PDOInstrumentationTest.php
+++ src/Instrumentation/PDO/tests/Integration/PDOInstrumentationTest.php
@@ -78,14 +78,58 @@ class PDOInstrumentationTest extends TestCase
         $statement = self::fillDB();
 
         $db->exec($statement);
+        $span = $this->storage->offsetGet(1);
+        $this->assertSame('PDO::exec', $span->getName());
+
+        $this->assertSame($db->inTransaction(), false);
         $this->assertCount(2, $this->storage);
 
         $sth = $db->prepare('SELECT * FROM `technology`');
-        $sth->execute();
+        $span = $this->storage->offsetGet(2);
+        $this->assertSame('PDO::prepare', $span->getName());
         $this->assertCount(3, $this->storage);
+        $sth->execute();
+        $span = $this->storage->offsetGet(3);
+        $this->assertSame('PDOStatement::execute', $span->getName());
+        $this->assertCount(4, $this->storage);
 
         $result = $sth->fetchAll();
-        $this->assertCount(4, $this->storage);
-    }
+        $span = $this->storage->offsetGet(4);
+        $this->assertSame('PDOStatement::fetchAll', $span->getName());
+        $this->assertCount(5, $this->storage);
 
+        $db->query('SELECT * FROM `technology`');
+        $span = $this->storage->offsetGet(5);
+        $this->assertSame('PDO::query', $span->getName());
+        $this->assertCount(6, $this->storage);
+    }
+    public function test_transaction(): void
+    {
+        $db =  self::createDB();
+        $result = $db->beginTransaction();
+        $span = $this->storage->offsetGet(1);
+        $this->assertSame('PDO::beginTransaction', $span->getName());
+        $this->assertCount(2, $this->storage);
+        $this->assertSame($result, true);
+        $statement = self::fillDB();
+        $db->exec($statement);
+        $result = $db->commit();
+        $span = $this->storage->offsetGet(3);
+        $this->assertSame('PDO::commit', $span->getName());
+        $this->assertCount(4, $this->storage);
+        $this->assertSame($result, true);
+        $result = $db->beginTransaction();
+        $this->assertSame($result, true);
+        $this->assertSame($db->inTransaction(), true);
+        $db->exec("INSERT INTO technology(`name`, `date`) VALUES('Java', '1995-05-23');");
+        $result = $db->rollback();
+        $span = $this->storage->offsetGet(6);
+        $this->assertSame('PDO::rollBack', $span->getName());
+        $this->assertCount(7, $this->storage);
+        $this->assertSame($result, true);
+        $this->assertSame($db->inTransaction(), false);
+        $sth = $db->prepare('SELECT * FROM `technology`');
+        $sth->execute();
+        $this->assertSame(count($sth->fetchAll()), 2);
+    }
 }
