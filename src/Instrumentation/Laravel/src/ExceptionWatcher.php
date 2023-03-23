111f5c77b91b28402d88aa88b2c661f49a947ef8
Przemek Delewski <pdelewski@sumologic.com>
2023-03-23 11:23:09 +0100

    adding ExceptionWatcher

---
+++ src/Instrumentation/Laravel/src/ExceptionWatcher.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Contrib\Instrumentation\Laravel;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Log\Events\MessageLogged;
+use OpenTelemetry\API\Trace\Span;
+use OpenTelemetry\API\Trace\StatusCode;
+use OpenTelemetry\Context\Context;
+use OpenTelemetry\SemConv\TraceAttributes;
+
+class ExceptionWatcher extends Watcher
+{
+    /** @psalm-suppress UndefinedInterfaceMethod */
+    public function register(Application $app): void
+    {
+        $app['events']->listen(MessageLogged::class, [$this, 'recordException']);
+    }
+    /**
+     * Record an exception.
+     */
+    public function recordException(MessageLogged $log): void
+    {
+        $exception = $log->context['exception'];
+
+        $attributes = [
+            'code.namespace' => get_class($exception),
+            'code.filepath' => $exception->getFile(),
+            'code.lineno' => $exception->getLine(),
+        ];
+        $scope = Context::storage()->scope();
+        if (!$scope) {
+            return;
+        }
+        $span = Span::fromContext($scope->context());
+        $span->recordException($exception, [TraceAttributes::EXCEPTION_ESCAPED => true]);
+        $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
+    }
+}
--- src/Instrumentation/Laravel/src/LaravelInstrumentation.php
+++ src/Instrumentation/Laravel/src/LaravelInstrumentation.php
@@ -93,6 +93,7 @@ class LaravelInstrumentation
             pre: static function (ServiceProvider $provider, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($instrumentation) {
                 if (!self::$watchersInstalled) {
                     self::registerWatchers(self::$application, new ClientRequestWatcher($instrumentation));
+                    self::registerWatchers(self::$application, new ExceptionWatcher());
                     self::$watchersInstalled = true;
                 }
             },
