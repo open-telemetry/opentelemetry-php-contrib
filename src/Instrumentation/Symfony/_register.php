bd71cab92f3c741913f05b99d5bc8b871a549804
Piotr Antosik <mail@piotrantosik.com>
2023-03-30 08:23:54 +0200

    code style fix

--- src/Instrumentation/Symfony/_register.php
+++ src/Instrumentation/Symfony/_register.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use OpenTelemetry\Contrib\Instrumentation\Symfony\SymfonyInstrumentation;
 use OpenTelemetry\Contrib\Instrumentation\Symfony\HttpClientInstrumentation;
+use OpenTelemetry\Contrib\Instrumentation\Symfony\SymfonyInstrumentation;
 
 if (extension_loaded('otel_instrumentation') === false) {
     trigger_error('The otel_instrumentation extension must be loaded in order to autoload the OpenTelemetry Laravel auto-instrumentation', E_USER_WARNING);
