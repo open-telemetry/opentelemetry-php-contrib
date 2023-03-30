f6e3f51a1eecc2979ad110cbd78522b8bf86b86f
Sem Schilder <sem@tropical.email>
2023-03-30 11:41:20 +0200

    Introduce warning

--- src/Instrumentation/Psr15/_register.php
+++ src/Instrumentation/Psr15/_register.php
@@ -9,4 +9,10 @@ if (class_exists(Sdk::class) && Sdk::isInstrumentationDisabled(Psr15Instrumentat
     return;
 }
 
+if (extension_loaded('otel_instrumentation') === false) {
+    trigger_error('The otel_instrumentation extension must be loaded in order to autoload the OpenTelemetry PSR-15 auto-instrumentation', E_USER_WARNING);
+
+    return;
+}
+
 Psr15Instrumentation::register();
