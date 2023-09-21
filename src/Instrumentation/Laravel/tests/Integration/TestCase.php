f316ccbf2400b5da6de4940772c5a6fa8632aeaf
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2023-09-21 20:03:19 +0100

    Laravel: removed test app skeleton.

--- src/Instrumentation/Laravel/tests/Integration/TestCase.php
+++ src/Instrumentation/Laravel/tests/Integration/TestCase.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Tests\Instrumentation\Laravel;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
 }
