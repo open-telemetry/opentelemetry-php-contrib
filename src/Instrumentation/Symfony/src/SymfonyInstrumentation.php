f6dad3acf4b3011497ec7d9affe7e4a2264507e7
Cedric Ziel <cedric.ziel@grafana.com>
2023-10-19 22:03:41 +0200

    Update span name if route is known

--- src/Instrumentation/Symfony/src/SymfonyInstrumentation.php
+++ src/Instrumentation/Symfony/src/SymfonyInstrumentation.php
@@ -83,7 +83,9 @@ final class SymfonyInstrumentation
                     $routeName = $request->attributes->get('_route', '');
 
                     if ('' !== $routeName) {
-                        $span->setAttribute(TraceAttributes::HTTP_ROUTE, $routeName);
+                        $span
+                            ->updateName(sprintf('%s %s', $request->getMethod(), $routeName))
+                            ->setAttribute(TraceAttributes::HTTP_ROUTE, $routeName);
                     }
                 }
 
