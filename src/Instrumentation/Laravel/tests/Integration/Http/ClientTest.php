73501bbb22406043087ebc53190bbaaa80b2253d
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2023-11-16 16:14:15 +0000

    Laravel: fixed ClientTest after TraceAttribute changes.

--- src/Instrumentation/Laravel/tests/Integration/Http/ClientTest.php
+++ src/Instrumentation/Laravel/tests/Integration/Http/ClientTest.php
@@ -26,14 +26,14 @@ class ClientTest extends TestCase
         $response = Http::get('missing.opentelemetry.io');
         $span = $this->storage[0];
         self::assertEquals(404, $response->status());
-        self::assertEquals('HTTP GET', $span->getName());
-        self::assertEquals('missing.opentelemetry.io', $span->getAttributes()->get(TraceAttributes::HTTP_TARGET));
+        self::assertEquals('GET', $span->getName());
+        self::assertEquals('missing.opentelemetry.io', $span->getAttributes()->get(TraceAttributes::URL_PATH));
 
         $response = Http::post('ok.opentelemetry.io/foo?param=bar');
         $span = $this->storage[1];
         self::assertEquals(201, $response->status());
-        self::assertEquals('HTTP POST', $span->getName());
-        self::assertEquals('ok.opentelemetry.io/foo', $span->getAttributes()->get(TraceAttributes::HTTP_TARGET));
+        self::assertEquals('POST', $span->getName());
+        self::assertEquals('ok.opentelemetry.io/foo', $span->getAttributes()->get(TraceAttributes::URL_PATH));
     }
 
     /** @test */
@@ -47,8 +47,8 @@ class ClientTest extends TestCase
         }
 
         $span = $this->storage[0];
-        self::assertEquals('HTTP PATCH', $span->getName());
-        self::assertEquals('http://fail', $span->getAttributes()->get(TraceAttributes::HTTP_URL));
+        self::assertEquals('PATCH', $span->getName());
+        self::assertEquals('http://fail', $span->getAttributes()->get(TraceAttributes::URL_FULL));
         self::assertEquals(StatusData::create(StatusCode::STATUS_ERROR, 'Connection failed'), $span->getStatus());
     }
 }
