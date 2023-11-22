f3e21f4db10d04ca6b1c6a3e2d135d395f10eea9
Ago Allikmaa <ago@allikmaa.eu>
2023-11-22 12:24:27 +0200

    Yii2 framework instrumentation (#212)
    * Yii2 instrumentation
    * Address PR comments.
    * Fix psalm issue

---
+++ src/Instrumentation/Yii/tests/Integration/YiiInstrumentationTest.php
@@ -0,0 +1,164 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Tests\Instrumentation\Yii\tests\Integration;
+
+use OpenTelemetry\SemConv\TraceAttributes;
+use yii\web\Application;
+use yii\web\Controller;
+
+class YiiInstrumentationTest extends AbstractTest
+{
+    public function test_success()
+    {
+        $exception = $this->runRequest('/site/index');
+
+        // Throws on success because headers cannot be sent
+        $this->assertNotNull($exception);
+        $this->assertEquals('yii\web\HeadersAlreadySentException', get_class($exception));
+
+        $attributes = $this->storage[0]->getAttributes();
+        $this->assertCount(1, $this->storage);
+        $this->assertEquals('SiteController.actionIndex', $this->storage[0]->getName());
+        $this->assertEquals('http://example.com/site/index', $attributes->get(TraceAttributes::URL_FULL));
+        $this->assertEquals('GET', $attributes->get(TraceAttributes::HTTP_REQUEST_METHOD));
+        $this->assertEquals('http', $attributes->get(TraceAttributes::URL_SCHEME));
+        $this->assertEquals('SiteController.actionIndex', $attributes->get(TraceAttributes::HTTP_ROUTE));
+        $this->assertEquals(200, $attributes->get(TraceAttributes::HTTP_RESPONSE_STATUS_CODE));
+        $this->assertEquals('1.1', $attributes->get(TraceAttributes::NETWORK_PROTOCOL_VERSION));
+        $this->assertGreaterThan(0, $attributes->get(TraceAttributes::HTTP_RESPONSE_BODY_SIZE));
+    }
+
+    public function test_exception()
+    {
+        $exception = $this->runRequest('/site/throw');
+
+        $this->assertNotNull($exception);
+        $this->assertEquals('Threw', $exception->getMessage());
+
+        $attributes = $this->storage[0]->getAttributes();
+        $this->assertCount(1, $this->storage);
+        $this->assertEquals('SiteController.actionThrow', $this->storage[0]->getName());
+        $this->assertEquals('http://example.com/site/throw', $attributes->get(TraceAttributes::URL_FULL));
+        $this->assertEquals('GET', $attributes->get(TraceAttributes::HTTP_REQUEST_METHOD));
+        $this->assertEquals('http', $attributes->get(TraceAttributes::URL_SCHEME));
+        $this->assertEquals('SiteController.actionThrow', $attributes->get(TraceAttributes::HTTP_ROUTE));
+    }
+
+    public function test_parent()
+    {
+        try {
+            $_SERVER['HTTP_TRACEPARENT'] = '00-0af7651916cd43dd8448eb211c80319c-b9c7c989f97918e1-01';
+            $exception = $this->runRequest('/site/index');
+        } finally {
+            unset($_SERVER['HTTP_TRACEPARENT']);
+        }
+
+        // Throws on success because headers cannot be sent
+        $this->assertNotNull($exception);
+        $this->assertEquals('yii\web\HeadersAlreadySentException', get_class($exception));
+
+        $span = $this->storage[0];
+        $this->assertEquals('0af7651916cd43dd8448eb211c80319c', $span->getTraceId());
+        $this->assertEquals('b9c7c989f97918e1', $span->getParentSpanId());
+
+        $attributes = $span->getAttributes();
+        $this->assertCount(1, $this->storage);
+        $this->assertEquals('SiteController.actionIndex', $this->storage[0]->getName());
+        $this->assertEquals('http://example.com/site/index', $attributes->get(TraceAttributes::URL_FULL));
+        $this->assertEquals('GET', $attributes->get(TraceAttributes::HTTP_REQUEST_METHOD));
+        $this->assertEquals('http', $attributes->get(TraceAttributes::URL_SCHEME));
+        $this->assertEquals('SiteController.actionIndex', $attributes->get(TraceAttributes::HTTP_ROUTE));
+        $this->assertEquals(200, $attributes->get(TraceAttributes::HTTP_RESPONSE_STATUS_CODE));
+        $this->assertEquals('1.1', $attributes->get(TraceAttributes::NETWORK_PROTOCOL_VERSION));
+        $this->assertGreaterThan(0, $attributes->get(TraceAttributes::HTTP_RESPONSE_BODY_SIZE));
+    }
+
+    private function runRequest($path)
+    {
+        $config = [
+            'id' => 'basic',
+            'basePath' => dirname(__DIR__),
+            'bootstrap' => ['log'],
+            'components' => [
+                'request' => [
+                    // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                    'cookieValidationKey' => 'ybQT_avit3_11aPJfJQ6zwbdjjwjOYbs',
+                ],
+                'cache' => [
+                    'class' => 'yii\caching\FileCache',
+                ],
+                'user' => [
+                    'identityClass' => 'app\models\User',
+                    'enableAutoLogin' => true,
+                ],
+                'errorHandler' => [
+                    'errorAction' => 'site/error',
+                ],
+                'log' => [
+                    'traceLevel' => 3,
+                    'targets' => [
+                        [
+                            'class' => 'yii\log\FileTarget',
+                            'levels' => ['error', 'warning'],
+                        ],
+                    ],
+                ],
+                'db' => [
+                    'class' => 'yii\db\Connection',
+                    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+                    'username' => 'root',
+                    'password' => '',
+                    'charset' => 'utf8',
+                ],
+                'urlManager' => [
+                    'enablePrettyUrl' => true,
+                    'showScriptName' => false,
+                    'rules' => [
+                        '<controller:\w+>' => '<controller>',
+                        '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                    ],
+                ],
+            ],
+            'controllerMap' => [
+                'site' => [
+                    'class' => 'OpenTelemetry\Tests\Instrumentation\Yii\tests\Integration\SiteController',
+                ],
+            ],
+            'params' => [],
+        ];
+
+        $_SERVER['SERVER_NAME'] = 'example.com';
+        $_SERVER['REQUEST_URI'] = $path;
+        $_SERVER['SCRIPT_NAME'] = '/';
+        $_SERVER['SCRIPT_FILENAME'] = '/';
+
+        $exception = null;
+
+        try {
+            (new Application($config))->run();
+        } catch (\Exception $e) {
+            $exception = $e;
+        }
+
+        return $exception;
+    }
+}
+
+class SiteController extends Controller
+{
+    public function actionIndex()
+    {
+        return \Yii::createObject([
+            'class' => 'yii\web\Response',
+            'format' => \yii\web\Response::FORMAT_RAW,
+            'content' => 'hello',
+        ]);
+    }
+
+    public function actionThrow()
+    {
+        throw new \Exception('Threw');
+    }
+}
