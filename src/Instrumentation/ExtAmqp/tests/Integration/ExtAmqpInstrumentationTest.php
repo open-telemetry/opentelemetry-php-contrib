3d70dde20b2ba52ef08a77c3d6cc1dc1d29374e8
Cedric Ziel <cedric@cedric-ziel.com>
2023-12-03 14:22:29 +0100

    Remove unused property

--- src/Instrumentation/ExtAmqp/tests/Integration/ExtAmqpInstrumentationTest.php
+++ src/Instrumentation/ExtAmqp/tests/Integration/ExtAmqpInstrumentationTest.php
@@ -23,7 +23,6 @@ class ExtAmqpInstrumentationTest extends TestCase
      * @var ImmutableSpan[]
      */
     private ArrayObject $storage;
-    private ImmutableSpan $span;
 
     public function setUp(): void
     {
