010fd1e98600211aaf2466ff7f0a9e933b445c71
Sem Schilder <sem@tropical.email>
2023-12-12 08:50:06 +0100

    Add assert on psr14.event.name attribute

--- src/Instrumentation/Psr14/tests/Integration/Psr14InstrumentationTest.php
+++ src/Instrumentation/Psr14/tests/Integration/Psr14InstrumentationTest.php
@@ -2,12 +2,14 @@
 
 declare(strict_types=1);
 
-namespace OpenTelemetry\Instrumentation\Psr14\tests\Integration;
+namespace OpenTelemetry\Instrumentation\Psr14\Tests\Integration;
 
 use ArrayObject;
 use OpenTelemetry\API\Instrumentation\Configurator;
 use OpenTelemetry\API\Trace\Propagation\TraceContextPropagator;
 use OpenTelemetry\Context\ScopeInterface;
+use OpenTelemetry\Instrumentation\Psr14\Tests\Fixture\SampleEventClass;
+use OpenTelemetry\SDK\Trace\ImmutableSpan;
 use OpenTelemetry\SDK\Trace\SpanExporter\InMemoryExporter;
 use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
@@ -31,14 +33,7 @@ class Psr14InstrumentationTest extends TestCase
                 new InMemoryExporter($this->storage)
             )
         );
-        $this->sampleEvent = new class() {
-            public string $item;
-            public function __construct(
-                string $item = 'test',
-            ) {
-                $this->item = $item;
-            }
-        };
+        $this->sampleEvent = new SampleEventClass();
 
         $this->scope = Configurator::create()
            ->withTracerProvider($this->tracerProvider)
@@ -63,5 +58,11 @@ class Psr14InstrumentationTest extends TestCase
         $this->assertCount(0, $this->storage);
         $this->dispatcher->dispatch($this->sampleEvent);
         $this->assertCount(1, $this->storage);
+
+        /** @var ImmutableSpan $span */
+        $span = $this->storage[0];
+        var_dump($span->getAttributes());
+
+        $this->assertEquals(SampleEventClass::class, $span->getAttributes()->get('psr14.event.name'));
     }
 }
