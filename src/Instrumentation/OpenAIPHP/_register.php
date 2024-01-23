4bc1a57549e2cc661d9400dae8cf8f0ea42e746a
Cedric Ziel <cedric.ziel@grafana.com>
2024-01-23 09:04:02 +0100

    _register.php aktualisieren
    Co-authored-by: Ago Allikmaa <ago@allikmaa.eu>

--- src/Instrumentation/OpenAIPHP/_register.php
+++ src/Instrumentation/OpenAIPHP/_register.php
@@ -10,7 +10,7 @@ if (class_exists(Sdk::class) && Sdk::isInstrumentationDisabled(OpenAIPHPInstrume
 }
 
 if (extension_loaded('opentelemetry') === false) {
-    trigger_error('The opentelemetry extension must be loaded in order to autoload the OpenTelemetry ext-amqp auto-instrumentation', E_USER_WARNING);
+    trigger_error('The opentelemetry extension must be loaded in order to autoload the OpenTelemetry OpenAI auto-instrumentation', E_USER_WARNING);
 
     return;
 }
