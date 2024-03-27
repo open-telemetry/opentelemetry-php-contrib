04837f511e52f9ab2f89ab4bb347e9b519663195
Weslen Teche <weslen_teche@hotmail.com>
2024-03-27 20:01:45 -0300

    update tests

--- src/Instrumentation/Psr16/tests/Integration/Psr16InstrumentationTest.php
+++ src/Instrumentation/Psr16/tests/Integration/Psr16InstrumentationTest.php
@@ -8,9 +8,11 @@ use ArrayObject;
 use OpenTelemetry\API\Instrumentation\Configurator;
 use OpenTelemetry\API\Trace\Propagation\TraceContextPropagator;
 use OpenTelemetry\Context\ScopeInterface;
+use OpenTelemetry\SDK\Trace\ImmutableSpan;
 use OpenTelemetry\SDK\Trace\SpanExporter\InMemoryExporter;
 use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
+use OpenTelemetry\SemConv\TraceAttributes;
 use PHPUnit\Framework\TestCase;
 use Psr\SimpleCache\CacheInterface;
 
@@ -19,6 +21,7 @@ class Psr16InstrumentationTest extends TestCase
     private ScopeInterface $scope;
     private ArrayObject $storage;
     private TracerProvider $tracerProvider;
+    private CacheInterface $driver;
 
     public function setUp(): void
     {
@@ -32,6 +35,7 @@ class Psr16InstrumentationTest extends TestCase
             ->withTracerProvider($this->tracerProvider)
             ->withPropagator(new TraceContextPropagator())
             ->activate();
+        $this->driver = $this->createMemoryCacheDriver();
     }
 
     public function tearDown(): void
@@ -42,9 +46,13 @@ class Psr16InstrumentationTest extends TestCase
     public function test_get_key(): void
     {
         $this->assertCount(0, $this->storage);
-        $driver = $this->createMemoryCacheDriver();
-        $driver->get('foo');
+        $this->driver->get('foo');
         $this->assertCount(1, $this->storage);
+        /** @var ImmutableSpan $span */
+        $span = $this->storage->offsetGet(0);
+        $this->assertEquals('get', $span->getName());
+        $this->assertEquals('psr16', $span->getAttributes()->get(TraceAttributes::DB_SYSTEM));
+        $this->assertEquals('get', $span->getAttributes()->get(TraceAttributes::DB_OPERATION));
     }
 
     public function test_set_key(): void
@@ -53,11 +61,41 @@ class Psr16InstrumentationTest extends TestCase
         $driver = $this->createMemoryCacheDriver();
         $driver->set('foo', 'bar');
         $this->assertCount(1, $this->storage);
+        $span = $this->storage->offsetGet(0);
+        $this->assertEquals('set', $span->getName());
+        $this->assertEquals('psr16', $span->getAttributes()->get(TraceAttributes::DB_SYSTEM));
+        $this->assertEquals('set', $span->getAttributes()->get(TraceAttributes::DB_OPERATION));
+    }
+
+    public function test_delete_key(): void
+    {
+        $this->assertCount(0, $this->storage);
+        $driver = $this->createMemoryCacheDriver();
+        $driver->set('foo', 'bar');
+        $driver->delete('foo');
+        $this->assertCount(2, $this->storage);
+        $span = $this->storage->offsetGet(1);
+        $this->assertEquals('delete', $span->getName());
+        $this->assertEquals('psr16', $span->getAttributes()->get(TraceAttributes::DB_SYSTEM));
+        $this->assertEquals('delete', $span->getAttributes()->get(TraceAttributes::DB_OPERATION));
+    }
+
+    public function test_clear_keys(): void
+    {
+        $this->assertCount(0, $this->storage);
+        $driver = $this->createMemoryCacheDriver();
+        $driver->set('foo', 'bar');
+        $driver->clear();
+        $this->assertCount(2, $this->storage);
+        $span = $this->storage->offsetGet(1);
+        $this->assertEquals('clear', $span->getName());
+        $this->assertEquals('psr16', $span->getAttributes()->get(TraceAttributes::DB_SYSTEM));
+        $this->assertEquals('clear', $span->getAttributes()->get(TraceAttributes::DB_OPERATION));
     }
 
     private function createMemoryCacheDriver(): CacheInterface
     {
-        return new class implements CacheInterface {
+        return new class() implements CacheInterface {
             private $data = [];
 
             public function get($key, $default = null): mixed
@@ -68,18 +106,21 @@ class Psr16InstrumentationTest extends TestCase
             public function set($key, $value, $ttl = null): bool
             {
                 $this->data[$key] = $value;
+
                 return true;
             }
 
             public function delete($key): bool
             {
                 unset($this->data[$key]);
+
                 return true;
             }
 
             public function clear(): bool
             {
                 $this->data = [];
+
                 return true;
             }
 
@@ -89,6 +130,7 @@ class Psr16InstrumentationTest extends TestCase
                 foreach ($keys as $key) {
                     $values[$key] = $this->get($key, $default);
                 }
+
                 return $values;
             }
 
@@ -97,6 +139,7 @@ class Psr16InstrumentationTest extends TestCase
                 foreach ($values as $key => $value) {
                     $this->set($key, $value, $ttl);
                 }
+
                 return true;
             }
 
@@ -105,12 +148,13 @@ class Psr16InstrumentationTest extends TestCase
                 foreach ($keys as $key) {
                     $this->delete($key);
                 }
+
                 return true;
             }
 
             public function has($key): bool
             {
-                return isset($this->data[$key]);
+                return !empty($this->get($key));
             }
         };
     }
