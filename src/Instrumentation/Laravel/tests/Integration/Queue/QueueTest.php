91414413ad133c4608aadbed509a20b003ce94d3
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2024-04-14 17:42:03 +0100

    Laravel: update QueueTest.

--- src/Instrumentation/Laravel/tests/Integration/Queue/QueueTest.php
+++ src/Instrumentation/Laravel/tests/Integration/Queue/QueueTest.php
@@ -11,11 +11,20 @@ use OpenTelemetry\Tests\Contrib\Instrumentation\Laravel\Integration\TestCase;
 class QueueTest extends TestCase
 {
     /** @test */
-    public function it_traces_jobs(): void
+    public function it_handles_pushing_to_a_queue(): void
     {
         /** @var Queue $queue */
         $queue = $this->app['queue'];
 
         $queue->push(new DummyJob('A'));
+        $queue->push(function () {
+            logger()->info('Logged from closure');
+        });
+
+        $this->assertEquals('sync process', $this->storage[0]->getName());
+        $this->assertEquals('Task: A', $this->storage[0]->getEvents()[0]->getName());
+
+        $this->assertEquals('sync process', $this->storage[1]->getName());
+        $this->assertEquals('Logged from closure', $this->storage[1]->getEvents()[0]->getName());
     }
 }
