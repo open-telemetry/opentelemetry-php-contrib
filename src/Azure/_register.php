d3ee9af97b12f63dac56f77e8a0f274f21618d14
Matthew Hensley <matthew.hensley@grafana.com>
2024-04-16 15:05:42 -0400

    provide client and req factory

--- src/Azure/_register.php
+++ src/Azure/_register.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 use OpenTelemetry\SDK\Registry;
 
+$client = new \GuzzleHttp\Client();
+$requestFactory = new \GuzzleHttp\Psr7\HttpFactory();
+
 Registry::registerResourceDetector('azure_app_service', new OpenTelemetry\Azure\AppService\Detector());
 Registry::registerResourceDetector('azure_container_apps', new OpenTelemetry\Azure\ContainerApps\Detector());
-Registry::registerResourceDetector('azure_vm', new OpenTelemetry\Azure\Vm\Detector());
+Registry::registerResourceDetector('azure_vm', new OpenTelemetry\Azure\Vm\Detector($client, $requestFactory));
