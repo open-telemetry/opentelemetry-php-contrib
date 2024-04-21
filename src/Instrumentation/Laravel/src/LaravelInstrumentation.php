6a8339621f47329d2752cbe76ad350b6408ee4f3
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2024-04-22 00:20:27 +0100

    Laravel: moved Console/Kernel/Command hooks.

--- src/Instrumentation/Laravel/src/LaravelInstrumentation.php
+++ src/Instrumentation/Laravel/src/LaravelInstrumentation.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace OpenTelemetry\Contrib\Instrumentation\Laravel;
 
 use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
+use OpenTelemetry\SDK\Common\Configuration\Configuration;
 
 class LaravelInstrumentation
 {
@@ -14,14 +15,23 @@ class LaravelInstrumentation
     {
         $instrumentation = new CachedInstrumentation('io.opentelemetry.contrib.php.laravel');
 
-        ConsoleInstrumentation::register($instrumentation);
         HttpInstrumentation::register($instrumentation);
 
+        Hooks\Illuminate\Console\Command::hook($instrumentation);
+        Hooks\Illuminate\Contracts\Console\Kernel::hook($instrumentation);
         Hooks\Illuminate\Contracts\Queue\Queue::hook($instrumentation);
         Hooks\Illuminate\Foundation\Application::hook($instrumentation);
         Hooks\Illuminate\Foundation\Console\ServeCommand::hook($instrumentation);
         Hooks\Illuminate\Queue\SyncQueue::hook($instrumentation);
         Hooks\Illuminate\Queue\Queue::hook($instrumentation);
         Hooks\Illuminate\Queue\Worker::hook($instrumentation);
+    }
+
+    public static function shouldTraceCli(): bool
+    {
+        return PHP_SAPI !== 'cli' || (
+            class_exists(Configuration::class)
+            && Configuration::getBoolean('OTEL_PHP_TRACE_CLI_ENABLED', false)
+        );
     }
 }
