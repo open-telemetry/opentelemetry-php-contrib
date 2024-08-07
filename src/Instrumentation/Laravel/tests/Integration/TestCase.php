dda3ae56e46880a98a1ed8187d6a5207033e3771
Hendrik Heil <hendrik@heil.sh>
2024-08-07 10:45:17 +0200

    feat(laravel-auto-instrumentation): use logger provider in logwatcher

--- src/Instrumentation/Laravel/tests/Integration/TestCase.php
+++ src/Instrumentation/Laravel/tests/Integration/TestCase.php
@@ -7,18 +7,23 @@ namespace OpenTelemetry\Tests\Contrib\Instrumentation\Laravel\Integration;
 use ArrayObject;
 use OpenTelemetry\API\Instrumentation\Configurator;
 use OpenTelemetry\Context\ScopeInterface;
+use OpenTelemetry\SDK\Logs\LoggerProvider;
+use OpenTelemetry\SDK\Logs\Processor\SimpleLogRecordProcessor;
+use OpenTelemetry\SDK\Logs\Exporter\InMemoryExporter as LogInMemoryExporter;
 use OpenTelemetry\SDK\Trace\ImmutableSpan;
-use OpenTelemetry\SDK\Trace\SpanExporter\InMemoryExporter;
+use OpenTelemetry\SDK\Trace\SpanExporter\InMemoryExporter as SpanInMemoryExporter;
 use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use OpenTelemetry\SDK\Logs\ReadWriteLogRecord;
 
 abstract class TestCase extends BaseTestCase
 {
     protected ScopeInterface $scope;
-    /** @var ArrayObject|ImmutableSpan[] $storage */
+    /** @var ArrayObject|ImmutableSpan[]|ReadWriteLogRecord $storage */
     protected ArrayObject $storage;
     protected TracerProvider $tracerProvider;
+    protected LoggerProvider $loggerProvider;
 
     public function setUp(): void
     {
@@ -27,12 +32,20 @@ abstract class TestCase extends BaseTestCase
         $this->storage = new ArrayObject();
         $this->tracerProvider = new TracerProvider(
             new SimpleSpanProcessor(
-                new InMemoryExporter($this->storage),
+                new SpanInMemoryExporter($this->storage),
             ),
         );
+        $this->loggerProvider = LoggerProvider::builder()
+            ->addLogRecordProcessor(
+                new SimpleLogRecordProcessor(
+                    new LogInMemoryExporter($this->storage),
+                ),
+            )
+            ->build();
 
         $this->scope = Configurator::create()
             ->withTracerProvider($this->tracerProvider)
+            ->withLoggerProvider($this->loggerProvider)
             ->activate();
     }
 
