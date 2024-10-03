0d7dcf47b7c9bea4737b2d75151cbe8102ecff66
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2024-10-03 23:13:26 +0100

    Laravel: remove property_exists check that supported Laravel versions do not require.

--- src/Instrumentation/Laravel/src/Hooks/Illuminate/Foundation/Console/ServeCommand.php
+++ src/Instrumentation/Laravel/src/Hooks/Illuminate/Foundation/Console/ServeCommand.php
@@ -28,10 +28,6 @@ class ServeCommand implements Hook
             FoundationServeCommand::class,
             'handle',
             preHook: static function (FoundationServeCommand $serveCommand, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($tracer) {
-                if (!property_exists(FoundationServeCommand::class, 'passthroughVariables')) {
-                    return;
-                }
-
                 foreach ($_ENV as $key => $value) {
                     if (str_starts_with($key, 'OTEL_') && !in_array($key, FoundationServeCommand::$passthroughVariables)) {
                         FoundationServeCommand::$passthroughVariables[] = $key;
