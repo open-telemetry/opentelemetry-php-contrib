c28ea776ff0407bee1038c644c75f4c26869c113
Flc <four_leaf_clover@foxmail.com>
2024-10-16 12:43:00 +0800

    fix(contrib-auto-laravel): fix log level is string but underlying logger expects int leading to incorrect types

--- src/Instrumentation/Laravel/src/Watchers/LogWatcher.php
+++ src/Instrumentation/Laravel/src/Watchers/LogWatcher.php
@@ -37,7 +37,9 @@ class LogWatcher extends Watcher
         $underlyingLogger = $this->logger->getLogger();
 
         /** @phan-suppress-next-line PhanUndeclaredMethod */
-        if (method_exists($underlyingLogger, 'isHandling') && !$underlyingLogger->isHandling($log->level)) {
+        if (method_exists($underlyingLogger, 'isHandling') && !$underlyingLogger->isHandling(
+                $underlyingLogger->toMonologLevel($log->level))
+        ) {
             return;
         }
 
