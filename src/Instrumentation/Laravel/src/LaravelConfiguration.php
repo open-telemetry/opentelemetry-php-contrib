f18ab42f7d1526d57b36cf6e87486247925e084d
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2024-10-28 22:29:40 +0000

    Laravel: update LaravelConfiguration enabled logic with default config.

--- src/Instrumentation/Laravel/src/LaravelConfiguration.php
+++ src/Instrumentation/Laravel/src/LaravelConfiguration.php
@@ -17,12 +17,15 @@ final class LaravelConfiguration implements InstrumentationConfiguration
     public static function fromArray(array $properties): self
     {
         return new self(
-            enabled: (class_exists(Sdk::class) && !Sdk::isInstrumentationDisabled('laravel')) || $properties['enabled'],
+            enabled: $properties['enabled'],
         );
     }
 
     public static function default(): self
     {
-        return self::fromArray([]);
+        return self::fromArray([
+            // Enabled by default if the OpenTelemetry SDK is not present. If it is, check whether disabled explicitly.
+            'enabled' => !class_exists(Sdk::class) || !Sdk::isInstrumentationDisabled('laravel'),
+        ]);
     }
 }
