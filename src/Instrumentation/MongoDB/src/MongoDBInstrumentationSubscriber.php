ee99079efa20aa2d1556852996888f3db28186d6
Brett McBride <brett.a.mcbride@gmail.com>
2025-01-14 19:34:01 +1100

    ext-mongodb 2.0 support (#324)
    Being able to retrieve host, port + info from a CommandStartedEvent is deprecated in 1.20.0 and will be removed in 2.0. we have been advised to use SDAM subscription instead.
    
    - update tests to allow defining mongo server from env
    - require ext-mongodb 1.13 (which provides SDAM subscriber for server info)
    - use SDAM subscriber to get server info attributes. Subscribe to the serverChanged event and store the attributes in a class variable, organised by host/port.
    - document mongo todos

--- src/Instrumentation/MongoDB/src/MongoDBInstrumentationSubscriber.php
+++ src/Instrumentation/MongoDB/src/MongoDBInstrumentationSubscriber.php
@@ -9,6 +9,16 @@ use MongoDB\Driver\Monitoring\CommandFailedEvent;
 use MongoDB\Driver\Monitoring\CommandStartedEvent;
 use MongoDB\Driver\Monitoring\CommandSubscriber;
 use MongoDB\Driver\Monitoring\CommandSucceededEvent;
+use MongoDB\Driver\Monitoring\SDAMSubscriber;
+use MongoDB\Driver\Monitoring\ServerChangedEvent;
+use MongoDB\Driver\Monitoring\ServerClosedEvent;
+use MongoDB\Driver\Monitoring\ServerHeartbeatFailedEvent;
+use MongoDB\Driver\Monitoring\ServerHeartbeatStartedEvent;
+use MongoDB\Driver\Monitoring\ServerHeartbeatSucceededEvent;
+use MongoDB\Driver\Monitoring\ServerOpeningEvent;
+use MongoDB\Driver\Monitoring\TopologyChangedEvent;
+use MongoDB\Driver\Monitoring\TopologyClosedEvent;
+use MongoDB\Driver\Monitoring\TopologyOpeningEvent;
 use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
 use OpenTelemetry\API\Trace\Span;
 use OpenTelemetry\API\Trace\SpanBuilderInterface;
@@ -18,13 +28,17 @@ use OpenTelemetry\Context\Context;
 use OpenTelemetry\SemConv\TraceAttributes;
 use Throwable;
 
-final class MongoDBInstrumentationSubscriber implements CommandSubscriber
+final class MongoDBInstrumentationSubscriber implements CommandSubscriber, SDAMSubscriber
 {
     private CachedInstrumentation $instrumentation;
     /**
      * @var Closure(object):?string
      */
     private Closure $commandSerializer;
+    /**
+     * @var array<string, array<int, array<string, mixed>>>
+     */
+    private array $serverAttributes = [];
 
     /**
      * @param (callable(object):?string) $commandSerializer
@@ -41,16 +55,26 @@ final class MongoDBInstrumentationSubscriber implements CommandSubscriber
         };
     }
 
+    /**
+     * @psalm-suppress MixedAssignment,MixedArrayTypeCoercion,MixedArrayOffset,MixedArgument
+     */
     public function commandStarted(CommandStartedEvent $event): void
     {
         $command = $event->getCommand();
         $collectionName = MongoDBCollectionExtractor::extract($command);
         $databaseName = $event->getDatabaseName();
         $commandName = $event->getCommandName();
-        $server = $event->getServer();
-        $info = $server->getInfo();
-        $port = $server->getPort();
-        $host = $server->getHost();
+        /** @phpstan-ignore-next-line */
+        if (version_compare(phpversion('mongodb'), '1.20.0', '>=')) {
+            $host = $event->getHost();
+            $port = $event->getPort();
+        } else {
+            $server = $event->getServer();
+            $host = $server->getHost();
+            $port = $server->getPort();
+        }
+        $attributes = $this->serverAttributes[$host][$port] ?? [];
+
         $isSocket = str_starts_with($host, '/');
         /** @psalm-suppress RiskyTruthyFalsyComparison **/
         $scopedCommand = ($collectionName ? $collectionName . '.' : '') . $commandName;
@@ -65,17 +89,10 @@ final class MongoDBInstrumentationSubscriber implements CommandSubscriber
             ->setAttribute(TraceAttributes::NETWORK_TRANSPORT, $isSocket ? 'unix' : 'tcp')
             ->setAttribute(TraceAttributes::DB_STATEMENT, ($this->commandSerializer)($command))
             ->setAttribute(TraceAttributes::DB_MONGODB_COLLECTION, $collectionName)
-            ->setAttribute(MongoDBTraceAttributes::DB_MONGODB_MASTER, $info['ismaster'] ?? null)
-            ->setAttribute(MongoDBTraceAttributes::DB_MONGODB_READ_ONLY, $info['readOnly'] ?? null)
-            ->setAttribute(MongoDBTraceAttributes::DB_MONGODB_CONNECTION_ID, $info['connectionId'] ?? null)
             ->setAttribute(MongoDBTraceAttributes::DB_MONGODB_REQUEST_ID, $event->getRequestId())
             ->setAttribute(MongoDBTraceAttributes::DB_MONGODB_OPERATION_ID, $event->getOperationId())
-            ->setAttribute(MongoDBTraceAttributes::DB_MONGODB_MAX_WIRE_VERSION, $info['maxWireVersion'] ?? null)
-            ->setAttribute(MongoDBTraceAttributes::DB_MONGODB_MIN_WIRE_VERSION, $info['minWireVersion'] ?? null)
-            ->setAttribute(MongoDBTraceAttributes::DB_MONGODB_MAX_BSON_OBJECT_SIZE_BYTES, $info['maxBsonObjectSize'] ?? null)
-            ->setAttribute(MongoDBTraceAttributes::DB_MONGODB_MAX_MESSAGE_SIZE_BYTES, $info['maxMessageSizeBytes'] ?? null)
-            ->setAttribute(MongoDBTraceAttributes::DB_MONGODB_MAX_WRITE_BATCH_SIZE, $info['maxWriteBatchSize'] ?? null);
-
+            ->setAttributes($attributes)
+        ;
         $parent = Context::getCurrent();
         $span = $builder->startSpan();
         Context::storage()->attach($span->storeInContext($parent));
@@ -117,5 +134,62 @@ final class MongoDBInstrumentationSubscriber implements CommandSubscriber
         }
 
         $span->end();
+    }
+
+    /**
+     * @todo In a load-balanced scenario, the hello response may be empty.
+     */
+    public function serverChanged(ServerChangedEvent $event): void
+    {
+        $host = $event->getHost();
+        $port = $event->getPort();
+        $info = $event->getNewDescription()->getHelloResponse();
+        $attributes = [
+            MongoDBTraceAttributes::DB_MONGODB_MASTER => $info['ismaster'] ?? null,
+            MongoDBTraceAttributes::DB_MONGODB_READ_ONLY => $info['readOnly'] ?? null,
+            MongoDBTraceAttributes::DB_MONGODB_CONNECTION_ID => $info['connectionId'] ?? null,
+            MongoDBTraceAttributes::DB_MONGODB_MAX_WIRE_VERSION => $info['maxWireVersion'] ?? null,
+            MongoDBTraceAttributes::DB_MONGODB_MIN_WIRE_VERSION => $info['minWireVersion'] ?? null,
+            MongoDBTraceAttributes::DB_MONGODB_MAX_BSON_OBJECT_SIZE_BYTES => $info['maxBsonObjectSize'] ?? null,
+            MongoDBTraceAttributes::DB_MONGODB_MAX_MESSAGE_SIZE_BYTES => $info['maxMessageSizeBytes'] ?? null,
+            MongoDBTraceAttributes::DB_MONGODB_MAX_WRITE_BATCH_SIZE => $info['maxWriteBatchSize'] ?? null,
+        ];
+        $this->serverAttributes[$host][$port] = $attributes;
+    }
+
+    public function serverOpened(ServerOpeningEvent $event): void
+    {
+    }
+
+    public function serverClosed(ServerClosedEvent $event): void
+    {
+    }
+
+    public function serverOpening(ServerOpeningEvent $event): void
+    {
+    }
+
+    public function serverHeartbeatFailed(ServerHeartbeatFailedEvent $event): void
+    {
+    }
+
+    public function serverHeartbeatStarted(ServerHeartbeatStartedEvent $event): void
+    {
+    }
+
+    public function serverHeartbeatSucceeded(ServerHeartbeatSucceededEvent $event): void
+    {
+    }
+
+    public function topologyChanged(TopologyChangedEvent $event): void
+    {
+    }
+
+    public function topologyClosed(TopologyClosedEvent $event): void
+    {
+    }
+
+    public function topologyOpening(TopologyOpeningEvent $event): void
+    {
     }
 }
