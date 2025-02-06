d77b8cd841f6e3c61344e6a231ac26800bf00e74
Brett McBride <brett@deakin.edu.au>
2025-02-06 16:10:58 +1100

    psr14

--- src/Instrumentation/Psr14/src/Psr14Instrumentation.php
+++ src/Instrumentation/Psr14/src/Psr14Instrumentation.php
@@ -11,6 +11,7 @@ use OpenTelemetry\API\Trace\StatusCode;
 use OpenTelemetry\Context\Context;
 use function OpenTelemetry\Instrumentation\hook;
 use OpenTelemetry\SemConv\TraceAttributes;
+use OpenTelemetry\SemConv\Version;
 use Psr\EventDispatcher\EventDispatcherInterface;
 
 use Throwable;
@@ -27,7 +28,7 @@ class Psr14Instrumentation
         $instrumentation = new CachedInstrumentation(
             'io.opentelemetry.contrib.php.psr14',
             InstalledVersions::getVersion('open-telemetry/opentelemetry-auto-psr14'),
-            'https://opentelemetry.io/schemas/1.24.0'
+            Version::VERSION_1_30_0->url(),
         );
 
         /**
@@ -40,10 +41,10 @@ class Psr14Instrumentation
                 $event = is_object($params[0]) ? $params[0] : null;
                 $builder = $instrumentation->tracer()
                    ->spanBuilder(sprintf('event %s', $event ? $event::class : 'unknown'))
-                   ->setAttribute(TraceAttributes::CODE_FUNCTION, $function)
+                   ->setAttribute(TraceAttributes::CODE_FUNCTION_NAME, $function)
                    ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
                    ->setAttribute(TraceAttributes::CODE_FILEPATH, $filename)
-                   ->setAttribute(TraceAttributes::CODE_LINENO, $lineno);
+                   ->setAttribute(TraceAttributes::CODE_LINE_NUMBER, $lineno);
 
                 if ($event) {
                     $builder->setAttribute('psr14.event.name', $event::class);
@@ -63,7 +64,7 @@ class Psr14Instrumentation
                 $span = Span::fromContext($scope->context());
 
                 if ($exception) {
-                    $span->recordException($exception, [TraceAttributes::EXCEPTION_ESCAPED => true]);
+                    $span->recordException($exception);
                     $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
                 }
 
