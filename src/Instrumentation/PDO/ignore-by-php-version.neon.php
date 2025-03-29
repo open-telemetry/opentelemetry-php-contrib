893fedd27417143e048299bdc6aaea20ca2b51e1
Cedric Ziel <cedric@cedric-ziel.com>
2025-03-29 10:46:08 +0100

    fix: phpstan conditional rules

--- src/Instrumentation/PDO/ignore-by-php-version.neon.php
+++ src/Instrumentation/PDO/ignore-by-php-version.neon.php
@@ -7,11 +7,10 @@ $ignoreErrors = [];
 if (version_compare(PHP_VERSION, '8.4', '<')) {
     $ignoreErrors = [
         '#Call to an undefined static method PDO::connect\(\)#',
-        '#PHPDoc tag @var for variable \$db contains unknown class PDO\\Sqlite#',
-        '#Call to method createFunction\(\) on an unknown class PDO\\Sqlite#',
-        '#Call to method exec\(\) on an unknown class PDO\\Sqlite#',
-        '#Call to method query\(\) on an unknown class PDO\\Sqlite#',
-        '#Call to an undefined method Pdo\\Sqlite::createFunction\(\)#',
+        '#PHPDoc tag @var for variable \$db contains unknown class PDO\\\\Sqlite#',
+        '#Call to method createFunction\(\) on an unknown class PDO\\\\Sqlite#',
+        '#Call to method exec\(\) on an unknown class PDO\\\\Sqlite#',
+        '#Call to method query\(\) on an unknown class PDO\\\\Sqlite#',
     ];
 }
 
