a2b5157082ca1e0f6d187c4a49bf92b9c484b56f
Brett McBride <brett@deakin.edu.au>
2025-05-01 13:27:56 +1000

    coverage

--- src/Instrumentation/Psr3/tests/Unit/FormatterTest.php
+++ src/Instrumentation/Psr3/tests/Unit/FormatterTest.php
@@ -6,12 +6,29 @@ namespace OpenTelemetry\Contrib\Psr3\tests\Unit;
 
 use Exception;
 use JsonSerializable;
+use OpenTelemetry\API\Behavior\Internal\Logging;
+use OpenTelemetry\API\Behavior\Internal\LogWriter\LogWriterInterface;
 use OpenTelemetry\Contrib\Instrumentation\Psr3\Formatter;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Stringable;
 
 class FormatterTest extends TestCase
 {
+    /** @var LogWriterInterface&MockObject */
+    private LogWriterInterface $logWriter;
+
+    public function setUp(): void
+    {
+        $this->logWriter = $this->createMock(LogWriterInterface::class);
+        Logging::setLogWriter($this->logWriter);
+    }
+
+    public function tearDown(): void
+    {
+        Logging::reset();
+    }
+
     public function test_format(): void
     {
         $context = [
@@ -30,6 +47,7 @@ class FormatterTest extends TestCase
                     return 'string_value';
                 }
             },
+            'b' => true,
         ];
         $formatted = Formatter::format($context);
         $this->assertSame('bar', $formatted['foo']);
@@ -39,5 +57,21 @@ class FormatterTest extends TestCase
         $this->assertSame('bar', $formatted['exception']['previous']['message']);
         $this->assertSame('string_value', $formatted['s']);
         $this->assertSame(['foo' => 'bar'], $formatted['j']);
+        $this->assertTrue($formatted['b']);
+    }
+
+    public function test_invalid_input_logs_warning(): void
+    {
+        $this->logWriter->expects($this->once())->method('write')->with(
+            $this->equalTo('warning'),
+            $this->stringContains('Failed to encode value'),
+        );
+        $context = [
+            'good' => 'foo',
+            'bad' => [fopen('php://memory', 'r+')], //resource cannot be encoded
+        ];
+        $formatted = Formatter::format($context);
+        $this->assertSame('foo', $formatted['good']);
+        $this->assertArrayNotHasKey('bad', $formatted);
     }
 }
