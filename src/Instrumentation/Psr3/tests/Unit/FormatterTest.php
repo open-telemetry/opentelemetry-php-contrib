37a9fe2c46d8b775e94c7a52e60a68a0810b2f1c
Brett McBride <brett@deakin.edu.au>
2025-05-01 13:14:15 +1000

    test coverage

--- src/Instrumentation/Psr3/tests/Unit/FormatterTest.php
+++ src/Instrumentation/Psr3/tests/Unit/FormatterTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Contrib\Psr3\tests\Unit;
 
+use Exception;
+use JsonSerializable;
 use OpenTelemetry\Contrib\Instrumentation\Psr3\Formatter;
 use PHPUnit\Framework\TestCase;
+use Stringable;
 
 class FormatterTest extends TestCase
 {
@@ -14,7 +17,19 @@ class FormatterTest extends TestCase
         $context = [
             0 => 'zero',
             'foo' => 'bar',
-            'exception' => new \Exception('foo', 500, new \RuntimeException('bar')),
+            'exception' => new Exception('foo', 500, new \RuntimeException('bar')),
+            'j' => new class() implements JsonSerializable {
+                public function jsonSerialize(): array
+                {
+                    return ['foo' => 'bar'];
+                }
+            },
+            's' => new class() implements Stringable {
+                public function __toString(): string
+                {
+                    return 'string_value';
+                }
+            },
         ];
         $formatted = Formatter::format($context);
         $this->assertSame('bar', $formatted['foo']);
@@ -22,5 +37,7 @@ class FormatterTest extends TestCase
         $this->assertSame('foo', $formatted['exception']['message']);
         $this->assertSame(500, $formatted['exception']['code']);
         $this->assertSame('bar', $formatted['exception']['previous']['message']);
+        $this->assertSame('string_value', $formatted['s']);
+        $this->assertSame(['foo' => 'bar'], $formatted['j']);
     }
 }
