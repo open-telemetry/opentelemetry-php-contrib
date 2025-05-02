aac44d4d53e1bda1e39609f497d1f7361880e1e3
Brett McBride <brett.a.mcbride@gmail.com>
2025-05-02 15:37:28 +1000

    improve psr3 log context formatting (#363)
    string values may contain data that causes export to choke, so:
    - json encode/decode strings and arrays that may contain strings, using JSON_INVALID_UTF8_SUBSTITUTE
    - only allow objects that are stringable or json serializable
    - allow other primitive types

--- src/Instrumentation/Psr3/tests/Unit/FormatterTest.php
+++ src/Instrumentation/Psr3/tests/Unit/FormatterTest.php
@@ -4,17 +4,50 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Contrib\Psr3\tests\Unit;
 
+use Exception;
+use JsonSerializable;
+use OpenTelemetry\API\Behavior\Internal\Logging;
+use OpenTelemetry\API\Behavior\Internal\LogWriter\LogWriterInterface;
 use OpenTelemetry\Contrib\Instrumentation\Psr3\Formatter;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Stringable;
 
 class FormatterTest extends TestCase
 {
+    /** @var LogWriterInterface&MockObject */
+    private LogWriterInterface $logWriter;
+
+    public function setUp(): void
+    {
+        $this->logWriter = $this->createMock(LogWriterInterface::class);
+        Logging::setLogWriter($this->logWriter);
+    }
+
+    public function tearDown(): void
+    {
+        Logging::reset();
+    }
+
     public function test_format(): void
     {
         $context = [
             0 => 'zero',
             'foo' => 'bar',
-            'exception' => new \Exception('foo', 500, new \RuntimeException('bar')),
+            'exception' => new Exception('foo', 500, new \RuntimeException('bar')),
+            'j' => new class() implements JsonSerializable {
+                public function jsonSerialize(): array
+                {
+                    return ['foo' => 'bar'];
+                }
+            },
+            's' => new class() implements Stringable {
+                public function __toString(): string
+                {
+                    return 'string_value';
+                }
+            },
+            'b' => true,
         ];
         $formatted = Formatter::format($context);
         $this->assertSame('bar', $formatted['foo']);
@@ -22,5 +55,23 @@ class FormatterTest extends TestCase
         $this->assertSame('foo', $formatted['exception']['message']);
         $this->assertSame(500, $formatted['exception']['code']);
         $this->assertSame('bar', $formatted['exception']['previous']['message']);
+        $this->assertSame('string_value', $formatted['s']);
+        $this->assertSame(['foo' => 'bar'], $formatted['j']);
+        $this->assertTrue($formatted['b']);
+    }
+
+    public function test_invalid_input_logs_warning(): void
+    {
+        $this->logWriter->expects($this->once())->method('write')->with(
+            $this->equalTo('warning'),
+            $this->stringContains('Failed to encode value'),
+        );
+        $context = [
+            'good' => 'foo',
+            'bad' => [fopen('php://memory', 'r+')], //resource cannot be encoded
+        ];
+        $formatted = Formatter::format($context);
+        $this->assertSame('foo', $formatted['good']);
+        $this->assertArrayNotHasKey('bad', $formatted);
     }
 }
