4e25e9e43b4695ca7ebadae5b3f074e3392296bc
Shawn Maddock <shawn@maddock.com>
2025-06-01 15:28:39 -0500

    Update src/Instrumentation/ReactPHP/src/ReactPHPInstrumentation.php
    Co-authored-by: Tobias Bachert <git@b-privat.de>

--- src/Instrumentation/ReactPHP/src/ReactPHPInstrumentation.php
+++ src/Instrumentation/ReactPHP/src/ReactPHPInstrumentation.php
@@ -181,7 +181,7 @@ class ReactPHPInstrumentation
                     ['ExplicitBucketBoundaries' => [0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10]]
                 );
 
-                if (!$span->isRecording() && is_a($histogram, NoopHistogram::class)) {
+                if (!$span->isRecording() && !$histogram->isEnabled()) {
                     return $promise;
                 }
 
