552df1f6c7db5b5b3fab3c4563ec1dcd4cc3c65e
Pawel Filipczak <pawel.filipczak@elastic.co>
2025-07-11 17:00:10 +0200

    PostgreSQL instrumentation

---
+++ src/Instrumentation/PostgreSql/src/PgSqlTracker.php
@@ -0,0 +1,208 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Contrib\Instrumentation\PostgreSql;
+
+use OpenTelemetry\API\Trace\SpanContextInterface;
+use OpenTelemetry\SemConv\TraceAttributes;
+use WeakMap;
+use WeakReference;
+use PgSql\Connection;
+
+/**
+ * @phan-file-suppress PhanNonClassMethodCall
+ */
+final class PgSqlTracker
+{
+
+    private WeakMap $connectionAttributes;
+    private WeakMap $connectionStatements;
+    public function __construct()
+    {
+        // /** @psalm-suppress PropertyTypeCoercion */
+        $this->connectionAttributes = new WeakMap();
+        $this->connectionStatements = new WeakMap();
+    }
+
+    public function addConnectionStatement(Connection $connection, string $statementName, string $query)
+    {
+        if (!$this->connectionStatements->offsetExists($connection)) {
+            $this->connectionStatements[$connection] = [];
+        }
+        $this->connectionStatements[$connection][$statementName] = $query;
+    }
+
+    public function getStatementQuery(Connection $connection, string $statementName) : ?string {
+        if ($this->connectionStatements->offsetExists($connection)) {
+            return $this->connectionStatements[$connection][$statementName] ?? null;
+        }
+        return null;
+    }
+
+    public function storeConnectionAttributes(Connection $connection, string $connectionString)
+    {
+        $this->connectionAttributes[$connection] = self::parseAttributesFromConnectionString($connectionString);
+    }
+    public function getConnectionAttributes(Connection $connection) : array
+    {
+        return $this->connectionAttributes[$connection] ?? [];
+    }
+    private function splitQueries(string $sql)
+    {
+        // Normalize line endings to \n
+        $sql = preg_replace("/\r\n|\n\r|\r/", "\n", $sql);
+        if ($sql === null) {
+            return [];
+        }
+
+        $queries = [];
+        $buffer = '';
+        $blockDepth = 0;
+        $tokens = preg_split('/(;)/', $sql, -1, PREG_SPLIT_DELIM_CAPTURE); // Keep semicolons as separate tokens
+
+        $singleQuotes = 0;
+        $doubleQuotes = 0;
+
+        if (empty($tokens)) {
+            return [];
+        }
+
+        foreach ($tokens as $token) {
+            if ($token === '') {
+                continue;
+            }
+
+            $tokenLen = strlen($token);
+            for ($i = 0; $i < $tokenLen; $i++) {
+                if ($token[$i] == "'" && ($token[$i - 1] ?? false) !== '\\') {
+                    $singleQuotes++;
+                }
+                if ($token[$i] == '"' && ($token[$i - 1] ?? false) !== '\\') {
+                    $doubleQuotes++;
+                }
+            }
+
+            $buffer .= $token;
+
+            // Detect BEGIN with optional label
+            if (preg_match('/(^|\s|[)])\bBEGIN\b/i', $token)) {
+                $blockDepth++;
+            }
+
+            // Detect END with optional label
+            if (preg_match('/\bEND\b(\s+[a-zA-Z0-9_]+)?\s*$/i', $token)) {
+                $blockDepth--;
+            }
+
+            // we're somewhere inside qoutes
+            if (($singleQuotes % 2) != 0 || ($doubleQuotes % 2) != 0) {
+                continue;
+            }
+
+            // If we are outside a block and encounter a semicolon, split the query
+            if ($blockDepth === 0 && $token === ';') {
+                $trimmedQuery = trim($buffer);
+                if ($trimmedQuery !== ';') { // Ignore empty queries
+                    $queries[] = $trimmedQuery;
+                }
+                $buffer = '';
+                $singleQuotes = 0;
+                $doubleQuotes = 0;
+            }
+        }
+
+        // Add any remaining buffer as a query
+        if (!empty(trim($buffer))) {
+            $queries[] = trim($buffer);
+        }
+
+        return $queries;
+    }
+
+
+    public static function parsePgConnString(string $conninfo): array {
+        $result = [];
+        $length = strlen($conninfo);
+        $i = 0;
+
+        while ($i < $length) {
+            // Skip leading whitespace
+            while ($i < $length && ctype_space($conninfo[$i])) {
+                $i++;
+            }
+
+            // Read the key until '=' or whitespace
+            $key = '';
+            while ($i < $length && $conninfo[$i] !== '=' && !ctype_space($conninfo[$i])) {
+                $key .= $conninfo[$i++];
+            }
+
+            // Skip whitespace before '='
+            while ($i < $length && ctype_space($conninfo[$i])) {
+                $i++;
+            }
+
+            // Expect '=' after key
+            if ($i >= $length || $conninfo[$i] !== '=') {
+                // throw new \InvalidArgumentException("Expected '=' after key '$key'");
+                //TODO verify - it might be socket name
+            }
+            $i++; // Move past '='
+
+            // Skip whitespace after '='
+            while ($i < $length && ctype_space($conninfo[$i])) {
+                $i++;
+            }
+
+            $value = '';
+
+            // Handle quoted value (single or double quotes)
+            if ($i < $length && ($conninfo[$i] === '\'' || $conninfo[$i] === '"')) {
+                $quote = $conninfo[$i++];
+                while ($i < $length) {
+                    if ($conninfo[$i] === '\\' && $i + 1 < $length) {
+                        // Handle escaped character
+                        $value .= $conninfo[++$i];
+                    } elseif ($conninfo[$i] === $quote) {
+                        // End of quoted value
+                        $i++;
+                        break;
+                    } else {
+                        $value .= $conninfo[$i++];
+                    }
+                }
+            } else {
+                // Handle unquoted value
+                while ($i < $length && !ctype_space($conninfo[$i])) {
+                    if ($conninfo[$i] === '\\' && $i + 1 < $length) {
+                        // Handle escaped character
+                        $value .= $conninfo[++$i];
+                    } else {
+                        $value .= $conninfo[$i++];
+                    }
+                }
+            }
+
+            // Only store non-empty keys
+            if ($key !== '') {
+                $result[$key] = $value;
+            }
+        }
+
+        return $result;
+    }
+
+    public static function parseAttributesFromConnectionString(string $connectionString) {
+        $connectionData = self::parsePgConnString($connectionString);
+
+        $addr = $connectionData['host'] ?? $connectionData['hostaddr'] ?? null;
+        $attributes = [];
+        $attributes[TraceAttributes::SERVER_ADDRESS] = $addr;
+        $attributes[TraceAttributes::SERVER_PORT] = $addr !== null ? ($connectionData['port'] ?? null) : null;
+        $attributes[TraceAttributes::DB_NAMESPACE] = $connectionData['dbname'] ?? $connectionData['user'] ?? null;
+        $attributes[TraceAttributes::DB_SYSTEM_NAME] =  'postgresql';
+        return $attributes;
+    }
+
+}
---
+++ src/Instrumentation/PostgreSql/src/PostgreSqlInstrumentation.php
@@ -0,0 +1,308 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\Contrib\Instrumentation\PostgreSql;
+
+use PgSql\Connection;
+use OpenTelemetry\API\Behavior\LogsMessagesTrait;
+use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
+use OpenTelemetry\API\Trace\Span;
+use OpenTelemetry\API\Trace\SpanInterface;
+use OpenTelemetry\API\Trace\SpanKind;
+use OpenTelemetry\API\Trace\StatusCode;
+
+use OpenTelemetry\Context\Context;
+use function OpenTelemetry\Instrumentation\hook;
+use OpenTelemetry\SemConv\TraceAttributes;
+use OpenTelemetry\SemConv\Version;
+
+/**
+ * @phan-file-suppress PhanParamTooFewUnpack
+ */
+class PostgreSqlInstrumentation
+{
+    use LogsMessagesTrait;
+
+    public const NAME = 'postgresql';
+    public static function register(): void
+    {
+
+//TODO Large objet - track by PgLob instance
+//pg_query_params, pg_select
+//pg_send_execute
+//pg_send_prepare
+//pg_send_query
+//pg_send_query_params
+
+        // https://opentelemetry.io/docs/specs/semconv/database/postgresql/
+        $instrumentation = new CachedInstrumentation(
+            'io.opentelemetry.contrib.php.postgresql',
+            null,
+            Version::VERSION_1_30_0->url(),
+        );
+
+        $tracker = new PgSqlTracker();
+
+        hook(
+            null,
+            'pg_connect',
+            pre: static function (...$args) use ($instrumentation, $tracker) {
+                self::connectPreHook('pg_connect', $instrumentation, $tracker, ...$args);
+            },
+            post: static function (...$args) use ($instrumentation, $tracker) {
+                self::connectPostHook($instrumentation, $tracker, ...$args);
+            }
+        );
+        hook(
+            null,
+            'pg_pconnect',
+            pre: static function (...$args) use ($instrumentation, $tracker) {
+                self::connectPreHook('pg_pconnect', $instrumentation, $tracker, ...$args);
+            },
+            post: static function (...$args) use ($instrumentation, $tracker) {
+                self::connectPostHook($instrumentation, $tracker, ...$args);
+            }
+        );
+        hook(
+            null,
+            'pg_convert',
+            pre: static function (...$args) use ($instrumentation, $tracker) {
+                self::basicPreHook('pg_convert', $instrumentation, $tracker, ...$args);
+            },
+            post: static function (...$args) use ($instrumentation, $tracker) {
+                self::tableOperationsPostHook($instrumentation, $tracker, true,  ...$args);
+            }
+        );
+
+
+
+        hook(
+            null,
+            'pg_copy_from',
+            pre: static function (...$args) use ($instrumentation, $tracker) {
+                self::basicPreHook('pg_copy_from', $instrumentation, $tracker, ...$args);
+            },
+            post: static function (...$args) use ($instrumentation, $tracker) {
+                self::tableOperationsPostHook($instrumentation, $tracker, false, ...$args);
+            }
+        );
+
+        hook(
+            null,
+            'pg_copy_to',
+            pre: static function (...$args) use ($instrumentation, $tracker) {
+                self::basicPreHook('pg_copy_to', $instrumentation, $tracker, ...$args);
+            },
+            post: static function (...$args) use ($instrumentation, $tracker) {
+                self::tableOperationsPostHook($instrumentation, $tracker, false, ...$args);
+            }
+        );
+
+        hook(
+            null,
+            'pg_delete',
+            pre: static function (...$args) use ($instrumentation, $tracker) {
+                self::basicPreHook('pg_delete', $instrumentation, $tracker, ...$args);
+            },
+            post: static function (...$args) use ($instrumentation, $tracker) {
+                self::tableOperationsPostHook($instrumentation, $tracker, false, ...$args);
+            }
+        );
+
+        hook(
+            null,
+            'pg_prepare',
+            pre: static function (...$args) use ($instrumentation, $tracker) {
+                self::basicPreHook('pg_prepare', $instrumentation, $tracker, ...$args);
+            },
+            post: static function (...$args) use ($instrumentation, $tracker) {
+                self::preparePostHook($instrumentation, $tracker, ...$args);
+            }
+        );
+
+        hook(
+            null,
+            'pg_execute',
+            pre: static function (...$args) use ($instrumentation, $tracker) {
+                self::basicPreHook('pg_execute', $instrumentation, $tracker, ...$args);
+            },
+            post: static function (...$args) use ($instrumentation, $tracker) {
+                self::executePostHook($instrumentation, $tracker, ...$args);
+            }
+        );
+
+
+        hook(
+            null,
+            'pg_query',
+            pre: static function (...$args) use ($instrumentation, $tracker) {
+                self::basicPreHook('pg_query', $instrumentation, $tracker, ...$args);
+            },
+            post: static function (...$args) use ($instrumentation, $tracker) {
+                self::queryPostHook($instrumentation, $tracker, ...$args);
+            }
+        );
+
+    }
+
+    /** @param non-empty-string $spanName */
+    private static function connectPreHook(string $spanName, CachedInstrumentation $instrumentation, PgSqlTracker $tracker, $obj, array $params, ?string $class, ?string $function, ?string $filename, ?int $lineno): void
+    {
+        $attributes = PgSqlTracker::parseAttributesFromConnectionString($params[0]);
+        self::startSpan($spanName, $instrumentation, $class, $function, $filename, $lineno, $attributes);
+    }
+
+    private static function connectPostHook(CachedInstrumentation $instrumentation, PgSqlTracker $tracker, $obj, array $params, mixed $retVal, ?\Throwable $exception)
+    {
+        if ($retVal instanceof Connection) {
+            $tracker->storeConnectionAttributes($retVal, $params[0]);
+        }
+        self::endSpan([], $exception, $retVal === false ? "Connection error" : null);
+    }
+
+    /** @param non-empty-string $spanName */
+    private static function basicPreHook(string $spanName, CachedInstrumentation $instrumentation, PgSqlTracker $tracker, $obj, array $params, ?string $class, ?string $function, ?string $filename, ?int $lineno): void
+    {
+        self::startSpan($spanName, $instrumentation, $class, $function, $filename, $lineno, []);
+    }
+
+    private static function basicPostHook(CachedInstrumentation $instrumentation, PgSqlTracker $tracker, ?array $attributes, bool $dropIfNoError, $obj, array $params, mixed $retVal, ?\Throwable $exception)
+    {
+        $errorStatus = $retVal === false ? pg_last_error($params[0]) : null;
+        if ($dropIfNoError && $errorStatus === null && $exception === null) {
+            self::dropSpan();
+            return;
+        }
+        self::endSpan($attributes, $exception, $errorStatus);
+    }
+
+    private static function tableOperationsPostHook(CachedInstrumentation $instrumentation, PgSqlTracker $tracker, bool $dropIfNoError, $obj, array $params, mixed $retVal, ?\Throwable $exception)
+    {
+        $connection = $params[0];
+        $attributes = null;
+        if ($connection instanceof Connection) {
+            $attributes = $tracker->getConnectionAttributes($connection);
+            $attributes[TraceAttributes::DB_NAMESPACE] = mb_convert_encoding($params[1], 'UTF-8');
+        }
+
+        $errorStatus = $retVal === false ? pg_last_error($params[0]) : null;
+        if ($dropIfNoError && $errorStatus === null && $exception === null) {
+            self::dropSpan();
+            return;
+        }
+        self::endSpan($attributes, $exception, $errorStatus);
+    }
+
+    private static function preparePostHook(CachedInstrumentation $instrumentation, PgSqlTracker $tracker, $obj, array $params, mixed $retVal, ?\Throwable $exception)
+    {
+        $attributes = $tracker->getConnectionAttributes($params[0]);
+
+        $attributes[TraceAttributes::DB_QUERY_TEXT] = mb_convert_encoding($params[2], 'UTF-8');
+        $attributes[TraceAttributes::DB_OPERATION_NAME] = self::extractQueryCommand($params[2]);
+
+        $errorStatus = $retVal === false ? pg_last_error($params[0]) : null;
+
+        if ($retVal !== false) {
+            $tracker->addConnectionStatement($params[0], $params[1], $params[2]);
+        }
+
+        self::endSpan($attributes, $exception, $errorStatus);
+    }
+
+    private static function executePostHook(CachedInstrumentation $instrumentation, PgSqlTracker $tracker, $obj, array $params, mixed $retVal, ?\Throwable $exception)
+    {
+        $attributes = $tracker->getConnectionAttributes($params[0]);
+
+        $query = $tracker->getStatementQuery($params[0], $params[1]);
+        if ($query !== null) {
+            $attributes[TraceAttributes::DB_QUERY_TEXT] = mb_convert_encoding($query, 'UTF-8');
+            $attributes[TraceAttributes::DB_OPERATION_NAME] = self::extractQueryCommand($query);
+        }
+
+        $errorStatus = $retVal === false ? pg_last_error($params[0]) : null;
+        self::endSpan($attributes, $exception, $errorStatus);
+    }
+
+    private static function queryPostHook(CachedInstrumentation $instrumentation, PgSqlTracker $tracker, $obj, array $params, mixed $retVal, ?\Throwable $exception)
+    {
+        $attributes = $tracker->getConnectionAttributes($params[0]);
+
+        $attributes[TraceAttributes::DB_QUERY_TEXT] = mb_convert_encoding($params[1], 'UTF-8');
+        $attributes[TraceAttributes::DB_OPERATION_NAME] = self::extractQueryCommand($params[1]);
+
+        $errorStatus = $retVal === false ? pg_last_error($params[0]) : null;
+        self::endSpan($attributes, $exception, $errorStatus);
+    }
+
+    /** @param non-empty-string $spanName */
+    private static function startSpan(string $spanName, CachedInstrumentation $instrumentation, ?string $class, ?string $function, ?string $filename, ?int $lineno, iterable $attributes) : SpanInterface
+    {
+        $parent = Context::getCurrent();
+        $builder = $instrumentation->tracer()
+            ->spanBuilder($spanName)
+            ->setParent($parent)
+            ->setSpanKind(SpanKind::KIND_CLIENT)
+            ->setAttribute(TraceAttributes::CODE_FUNCTION_NAME, $function)
+            ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
+            ->setAttribute(TraceAttributes::CODE_FILEPATH, $filename)
+            ->setAttribute(TraceAttributes::CODE_LINE_NUMBER, $lineno)
+            ->setAttributes($attributes);
+
+        $span = $builder->startSpan();
+        $context = $span->storeInContext($parent);
+
+        Context::storage()->attach($context);
+
+        return $span;
+    }
+
+    private static function endSpan(?iterable $attributes, ?\Throwable $exception, ?string $errorStatus)
+    {
+        $scope = Context::storage()->scope();
+        if (!$scope) {
+            return;
+        }
+        $scope->detach();
+        $span = Span::fromContext($scope->context());
+
+        if ($attributes) {
+            $span->setAttributes($attributes);
+        }
+
+        if ($errorStatus !== null) {
+            $span->setAttribute(TraceAttributes::EXCEPTION_MESSAGE, $errorStatus);
+            $span->setStatus(StatusCode::STATUS_ERROR, $errorStatus);
+        }
+
+        if ($exception) {
+            $span->recordException($exception);
+            $span->setAttribute(TraceAttributes::EXCEPTION_TYPE, $exception::class);
+            $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
+        }
+
+        $span->end();
+    }
+
+    private static function dropSpan()
+    {
+        $scope = Context::storage()->scope();
+        if (!$scope) {
+            return;
+        }
+        $scope->detach();
+    }
+
+    private static function extractQueryCommand($query) : ?string
+    {
+        $query = preg_replace("/\r\n|\n\r|\r/", "\n", $query);
+        /** @psalm-suppress PossiblyInvalidArgument */
+        if (preg_match('/^\s*(?:--[^\n]*\n|\/\*[\s\S]*?\*\/\s*)*([a-zA-Z_][a-zA-Z0-9_]*)/i', $query, $matches)) {
+            return strtoupper($matches[1]);
+        }
+
+        return null;
+    }
+
+
+}
