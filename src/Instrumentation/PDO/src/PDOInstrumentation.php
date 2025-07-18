574e7fc5add12c40ca73735ac19e3691ceb2e261
Jerry Leung <tingfung.leung@solarwinds.com>
2025-07-18 12:20:13 -0700

    nits

--- src/Instrumentation/PDO/src/PDOInstrumentation.php
+++ src/Instrumentation/PDO/src/PDOInstrumentation.php
@@ -24,7 +24,6 @@ class PDOInstrumentation
 {
     public const NAME = 'pdo';
     private const UNDEFINED = 'undefined';
-
     private const ALL = 'all';
 
     public static function register(): void
