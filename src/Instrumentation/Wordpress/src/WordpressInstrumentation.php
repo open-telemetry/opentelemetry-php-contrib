40eea2b152d2ff8261a647807ec16107b07e28e2
Sören Wünsch <soerenwrede@gmail.com>
2025-07-31 15:13:26 +0200

    WP: Don't set status ERROR on 404 response (#416)

--- src/Instrumentation/Wordpress/src/WordpressInstrumentation.php
+++ src/Instrumentation/Wordpress/src/WordpressInstrumentation.php
@@ -115,7 +115,6 @@ class WordpressInstrumentation
 
                     if (function_exists('is_404') && is_404()) {
                         $span->setAttribute(TraceAttributes::HTTP_RESPONSE_STATUS_CODE, 404);
-                        $span->setStatus(StatusCode::STATUS_ERROR);
                     }
                     //@todo check for other errors?
 
