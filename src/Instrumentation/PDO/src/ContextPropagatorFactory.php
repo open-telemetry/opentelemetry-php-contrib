ee9463993e664a4da5c16ab17e65467a9de60313
Jerry Leung <tingfung.leung@solarwinds.com>
2025-09-04 16:52:06 -0700

    Updated

--- src/Instrumentation/PDO/src/ContextPropagatorFactory.php
+++ src/Instrumentation/PDO/src/ContextPropagatorFactory.php
@@ -6,6 +6,7 @@ namespace OpenTelemetry\Contrib\Instrumentation\PDO;
 
 use OpenTelemetry\API\Behavior\LogsMessagesTrait;
 use OpenTelemetry\Context\Propagation\MultiTextMapPropagator;
+use OpenTelemetry\Context\Propagation\NoopTextMapPropagator;
 use OpenTelemetry\Context\Propagation\TextMapPropagatorInterface;
 use OpenTelemetry\SDK\Common\Configuration\Configuration;
 use OpenTelemetry\SDK\Registry;
@@ -25,7 +26,12 @@ class ContextPropagatorFactory
             case 0:
                 return null;
             case 1:
-                return $this->buildPropagator($propagators[0]);
+                $propagator = $this->buildPropagator($propagators[0]);
+                if (is_a($propagator, NoopTextMapPropagator::class)) {
+                    return null;
+                }
+
+                return $propagator;
             default:
                 $props = $this->buildPropagators($propagators);
                 if ($props) {
@@ -44,7 +50,7 @@ class ContextPropagatorFactory
         $propagators = [];
         foreach ($names as $name) {
             $propagator = $this->buildPropagator($name);
-            if ($propagator !== null) {
+            if ($propagator !== null && !is_a($propagator, NoopTextMapPropagator::class)) {
                 $propagators[] = $propagator;
             }
         }
