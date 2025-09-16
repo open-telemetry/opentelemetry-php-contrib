0e5a85812c629e6ba2a94a97e3235ce4af1e6151
Heena Bansal <139580694+HeenaBansal20@users.noreply.github.com>
2025-09-16 19:24:36 -0400

    Remove curl hooks from IO contrib package (#441)

--- src/Instrumentation/IO/tests/Integration/IOInstrumentationTest.php
+++ src/Instrumentation/IO/tests/Integration/IOInstrumentationTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace OpenTelemetry\Tests\Instrumentation\IO\tests\Integration;
 
 use ArrayObject;
-use CurlHandle;
 use OpenTelemetry\API\Instrumentation\Configurator;
 use OpenTelemetry\Context\ScopeInterface;
 use OpenTelemetry\SDK\Trace\ImmutableSpan;
@@ -75,17 +74,6 @@ class IOInstrumentationTest extends TestCase
         $this->span = $this->storage->offsetGet(4);
         $this->assertSame('fread', $this->span->getName());
 
-        $ch = curl_init('foo');
-        $this->assertInstanceOf(CurlHandle::class, $ch);
-        $this->assertCount(6, $this->storage);
-        $this->span = $this->storage->offsetGet(5);
-        $this->assertSame('curl_init', $this->span->getName());
-        $this->assertSame('foo', $this->span->getAttributes()->get('code.params.uri'));
-
-        curl_exec($ch);
-        $this->assertCount(7, $this->storage);
-        $this->span = $this->storage->offsetGet(6);
-        $this->assertSame('curl_exec', $this->span->getName());
     }
 
     public function test_output_buffer_calls(): void
