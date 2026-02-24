b01bbe39b550fdb86145039444fd6847be831cd4
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2026-02-24 00:27:57 +0000

    Revert changes in CommandTest to support Laravel < v11.27.1

--- src/Instrumentation/Laravel/tests/Integration/Console/CommandTest.php
+++ src/Instrumentation/Laravel/tests/Integration/Console/CommandTest.php
@@ -6,7 +6,6 @@ namespace OpenTelemetry\Tests\Contrib\Instrumentation\Laravel\Integration\Consol
 
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Foundation\Console\OptimizeClearCommand;
 use OpenTelemetry\Tests\Contrib\Instrumentation\Laravel\Integration\TestCase;
 
 /** @psalm-suppress UnusedClass */
@@ -32,11 +31,13 @@ class CommandTest extends TestCase
          */
         $this->assertCount(7, $this->storage);
 
-        $i = 0;
-        foreach ((new OptimizeClearCommand())->getOptimizeClearTasks() as $task) {
-            $this->assertSame(sprintf('Command %s', $task), $this->storage[$i++]->getName());
-        }
-        $this->assertSame('Command optimize:clear', $this->storage[$i]->getName());
+        $this->assertSame('Command event:clear', $this->storage[0]->getName());
+        $this->assertSame('Command view:clear', $this->storage[1]->getName());
+        $this->assertSame('Command cache:clear', $this->storage[2]->getName());
+        $this->assertSame('Command route:clear', $this->storage[3]->getName());
+        $this->assertSame('Command config:clear', $this->storage[4]->getName());
+        $this->assertSame('Command clear-compiled', $this->storage[5]->getName());
+        $this->assertSame('Command optimize:clear', $this->storage[6]->getName());
     }
 
     private function kernel(): Kernel
