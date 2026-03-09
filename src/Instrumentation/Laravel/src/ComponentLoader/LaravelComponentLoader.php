1fb578c5fc11941c854c1ec7f896a527bba12a49
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2026-03-09 20:42:59 +0000

    Laravel: lint LaravelComponentLoader

--- src/Instrumentation/Laravel/src/ComponentLoader/LaravelComponentLoader.php
+++ src/Instrumentation/Laravel/src/ComponentLoader/LaravelComponentLoader.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\Contrib\Instrumentation\Laravel\ComponentLoader;
 
+use function in_array;
 use OpenTelemetry\API\Configuration\ConfigEnv\EnvComponentLoader;
 use OpenTelemetry\API\Configuration\ConfigEnv\EnvComponentLoaderRegistry;
 use OpenTelemetry\API\Configuration\ConfigEnv\EnvResolver;
 use OpenTelemetry\API\Configuration\Context;
 use OpenTelemetry\API\Instrumentation\AutoInstrumentation\InstrumentationConfiguration;
 use OpenTelemetry\Contrib\Instrumentation\Laravel\LaravelConfiguration;
-use function in_array;
 
 /**
  * @implements EnvComponentLoader<InstrumentationConfiguration>
