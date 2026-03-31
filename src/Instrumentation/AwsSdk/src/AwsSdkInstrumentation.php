9c73e84e43740f671e2d35a6f9f8f333a0de369b
Sergei Zhidkov <53189193+adiafora@users.noreply.github.com>
2026-03-31 15:24:04 +0300

    fix(AwsSdk): handle missing x-amz-request-id in metadata (#524)
    * fix(AwsSdk): handle missing x-amz-request-id in metadata
    
    * fix(AwsSdk): Other possible headers have been added

--- src/Instrumentation/AwsSdk/src/AwsSdkInstrumentation.php
+++ src/Instrumentation/AwsSdk/src/AwsSdkInstrumentation.php
@@ -75,7 +75,17 @@ final class AwsSdkInstrumentation
 
                 if ($result instanceof ResultInterface && isset($result['@metadata'])) {
                     $span->setAttribute(TraceAttributes::HTTP_RESPONSE_STATUS_CODE, $result['@metadata']['statusCode']); // @phan-suppress-current-line PhanTypeMismatchDimFetch
-                    $span->setAttribute(TraceAttributes::AWS_REQUEST_ID, $result['@metadata']['headers']['x-amz-request-id']); // @phan-suppress-current-line PhanTypeMismatchDimFetch
+
+                    $headers = $result['@metadata']['headers'] ?? []; // @phan-suppress-current-line PhanTypeMismatchDimFetch
+
+                    $awsRequestId = $headers['x-amz-request-id']
+                        ?? $headers['x-amzn-requestid']
+                        ?? $headers['x-amzn-request-id']
+                        ?? null;
+
+                    if ($awsRequestId !== null) {
+                        $span->setAttribute(TraceAttributes::AWS_REQUEST_ID, $awsRequestId); // @phan-suppress-current-line PhanTypeMismatchDimFetch
+                    }
                 }
                 if ($ex) {
                     if ($ex instanceof AwsException && $ex->getAwsRequestId() !== null) {
