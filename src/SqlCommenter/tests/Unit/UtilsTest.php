95e8f7fa778ee82046817e7ee104a9e5cadf7fc8
Wes Campaigne <westacular@gmail.com>
2026-06-25 19:21:55 -0400

    Update UtilsTest.php

--- src/SqlCommenter/tests/Unit/UtilsTest.php
+++ src/SqlCommenter/tests/Unit/UtilsTest.php
@@ -21,16 +21,16 @@ class UtilsTest extends TestCase
 
     public function testFormatCommentsWithSpecialCharKeys(): void
     {
-        $this->assertEquals("/*key1='value1%%40',key2='value2'*/", Utils::formatComments(['key1' => 'value1@', 'key2' => 'value2']));
+        $this->assertEquals("/*key1='value1%40',key2='value2'*/", Utils::formatComments(['key1' => 'value1@', 'key2' => 'value2']));
     }
 
     public function testFormatCommentsWithPlaceholder(): void
     {
-        $this->assertEquals("/*key1='value1%%3F',key2='value2'*/", Utils::formatComments(['key1' => 'value1?', 'key2' => 'value2']));
+        $this->assertEquals("/*key1='value1%3F',key2='value2'*/", Utils::formatComments(['key1' => 'value1?', 'key2' => 'value2']));
     }
 
     public function testFormatCommentsWithNamedPlaceholder(): void
     {
-        $this->assertEquals("/*key1='%%3Anamed',key2='value2'*/", Utils::formatComments(['key1' => ':named', 'key2' => 'value2']));
+        $this->assertEquals("/*key1='%3Anamed',key2='value2'*/", Utils::formatComments(['key1' => ':named', 'key2' => 'value2']));
     }
 }
