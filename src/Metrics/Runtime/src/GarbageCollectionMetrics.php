fbaae9b5a64ae1d50537108260e629c94f51e33f
Tobias Bachert <git@b-privat.de>
2026-07-08 21:01:35 +0200

    Merge `gc_status()` callbacks to prevent inconsistent metrics if gc triggered in between callbacks

--- src/Metrics/Runtime/src/GarbageCollectionMetrics.php
+++ src/Metrics/Runtime/src/GarbageCollectionMetrics.php
@@ -6,6 +6,7 @@ namespace OpenTelemetry\Contrib\Metrics\Runtime;
 
 use OpenTelemetry\API\Metrics\MeterInterface;
 use OpenTelemetry\API\Metrics\ObserverInterface;
+use const PHP_VERSION_ID;
 
 /**
  * @internal
@@ -36,6 +37,26 @@ class GarbageCollectionMetrics
             '{object}',
             'Current number of objects in the root buffer',
         );
+        $collectorTime = $meter->createObservableCounter(
+            'php.gc.collector_time',
+            's',
+            'Cumulative time spent in the garbage collector',
+        );
+        $destructorTime = $meter->createObservableCounter(
+            'php.gc.destructor_time',
+            's',
+            'Cumulative time spent running destructors during GC',
+        );
+        $freeTime = $meter->createObservableCounter(
+            'php.gc.free_time',
+            's',
+            'Cumulative time spent freeing memory during GC',
+        );
+        $processUptime = $meter->createObservableGauge(
+            'process.uptime',
+            's',
+            'The time the process has been running',
+        );
 
         $meter->batchObserve(
             static function (
@@ -43,65 +64,39 @@ class GarbageCollectionMetrics
                 ObserverInterface $collectedObs,
                 ObserverInterface $thresholdObs,
                 ObserverInterface $rootsObs,
+                ObserverInterface $collectorObs,
+                ObserverInterface $destructorObs,
+                ObserverInterface $freeObs,
+                ObserverInterface $uptimeObs,
             ): void {
                 $status = gc_status();
                 $runsObs->observe($status['runs']);
                 $collectedObs->observe($status['collected']);
                 $thresholdObs->observe($status['threshold']);
                 $rootsObs->observe($status['roots']);
+
+                if (PHP_VERSION_ID < 80300) { // Timing metrics available since PHP 8.3
+                    return;
+                }
+
+                /** @var array<string, int|float> $status */
+                // @phan-suppress-next-line PhanTypeInvalidDimOffset, PhanTypeMismatchArgument -- fields added in PHP 8.3
+                $collectorObs->observe($status['collector_time']);
+                // @phan-suppress-next-line PhanTypeInvalidDimOffset, PhanTypeMismatchArgument -- fields added in PHP 8.3
+                $destructorObs->observe($status['destructor_time']);
+                // @phan-suppress-next-line PhanTypeInvalidDimOffset, PhanTypeMismatchArgument -- fields added in PHP 8.3
+                $freeObs->observe($status['free_time']);
+                // @phan-suppress-next-line PhanTypeInvalidDimOffset, PhanTypeMismatchArgument -- fields added in PHP 8.3
+                $uptimeObs->observe($status['application_time']);
             },
             $runs,
             $collected,
             $threshold,
             $roots,
+            $collectorTime,
+            $destructorTime,
+            $freeTime,
+            $processUptime,
         );
-
-        // Timing metrics available since PHP 8.3
-        if (PHP_VERSION_ID >= 80300) {
-            $collectorTime = $meter->createObservableCounter(
-                'php.gc.collector_time',
-                's',
-                'Cumulative time spent in the garbage collector',
-            );
-            $destructorTime = $meter->createObservableCounter(
-                'php.gc.destructor_time',
-                's',
-                'Cumulative time spent running destructors during GC',
-            );
-            $freeTime = $meter->createObservableCounter(
-                'php.gc.free_time',
-                's',
-                'Cumulative time spent freeing memory during GC',
-            );
-            $processUptime = $meter->createObservableGauge(
-                'process.uptime',
-                's',
-                'The time the process has been running',
-            );
-
-            $meter->batchObserve(
-                static function (
-                    ObserverInterface $collectorObs,
-                    ObserverInterface $destructorObs,
-                    ObserverInterface $freeObs,
-                    ObserverInterface $uptimeObs,
-                ): void {
-                    /** @var array<string, int|float> $status */
-                    $status = gc_status();
-                    // @phan-suppress-next-line PhanTypeInvalidDimOffset, PhanTypeMismatchArgument -- fields added in PHP 8.3
-                    $collectorObs->observe($status['collector_time']);
-                    // @phan-suppress-next-line PhanTypeInvalidDimOffset, PhanTypeMismatchArgument -- fields added in PHP 8.3
-                    $destructorObs->observe($status['destructor_time']);
-                    // @phan-suppress-next-line PhanTypeInvalidDimOffset, PhanTypeMismatchArgument -- fields added in PHP 8.3
-                    $freeObs->observe($status['free_time']);
-                    // @phan-suppress-next-line PhanTypeInvalidDimOffset, PhanTypeMismatchArgument -- fields added in PHP 8.3
-                    $uptimeObs->observe($status['application_time']);
-                },
-                $collectorTime,
-                $destructorTime,
-                $freeTime,
-                $processUptime,
-            );
-        }
     }
 }
