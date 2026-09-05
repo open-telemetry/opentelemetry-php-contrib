60979e88e0dd8e7813a6b9951785ee27d776a278
Application-drop-up <msts.oo0131@gmail.com>
2026-09-05 14:09:38 +0900

    test(laravel): cover concurrent identical requests in ClientRequestWatcher
    Sends two identical requests through Http::pool() and asserts each
    gets its own span correctly matched to its own response, reproducing
    the span collision fixed in the previous commit.

--- src/Instrumentation/Laravel/tests/Integration/Http/ClientTest.php
+++ src/Instrumentation/Laravel/tests/Integration/Http/ClientTest.php
@@ -6,10 +6,12 @@ namespace Integration\Http;
 
 use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Promise\RejectedPromise;
+use Illuminate\Http\Client\Pool;
 use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
 use OpenTelemetry\API\Trace\StatusCode;
 use OpenTelemetry\SDK\Trace\StatusData;
+use OpenTelemetry\SemConv\Attributes\HttpAttributes;
 use OpenTelemetry\SemConv\Attributes\UrlAttributes;
 use OpenTelemetry\Tests\Contrib\Instrumentation\Laravel\Integration\TestCase;
 
@@ -59,5 +61,33 @@ class ClientTest extends TestCase
         self::assertEquals('PATCH', $span->getName());
         self::assertEquals('http://fail', $span->getAttributes()->get(UrlAttributes::URL_FULL));
         self::assertEquals(StatusData::create(StatusCode::STATUS_ERROR, 'Connection failed'), $span->getStatus());
+    }
+
+    public function test_it_matches_spans_to_responses_for_concurrent_identical_requests(): void
+    {
+        Http::fake([
+            'same.opentelemetry.io' => Http::sequence()
+                ->push(status: 200)
+                ->push(status: 500),
+        ]);
+
+        $responses = Http::pool(fn (Pool $pool) => [
+            $pool->get('same.opentelemetry.io'),
+            $pool->get('same.opentelemetry.io'),
+        ]);
+
+        self::assertEquals(200, $responses[0]->status());
+        self::assertEquals(500, $responses[1]->status());
+
+        self::assertCount(2, $this->storage);
+
+        $firstSpan = $this->storage[0];
+        $secondSpan = $this->storage[1];
+
+        self::assertEquals(200, $firstSpan->getAttributes()->get(HttpAttributes::HTTP_RESPONSE_STATUS_CODE));
+        self::assertEquals(StatusCode::STATUS_UNSET, $firstSpan->getStatus()->getCode());
+
+        self::assertEquals(500, $secondSpan->getAttributes()->get(HttpAttributes::HTTP_RESPONSE_STATUS_CODE));
+        self::assertEquals(StatusCode::STATUS_ERROR, $secondSpan->getStatus()->getCode());
     }
 }
